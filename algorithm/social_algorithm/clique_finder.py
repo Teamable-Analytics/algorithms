197f196c7005648958f3b7b4a97e2fd00e8cd5c8
opeyem1a <opeyadeyemi@gmail.com>
2022-04-01 05:32:48 -0700

    + improve clique finder to only run expensive clique finding operation once + use different, faster algorithm for finding cliques

--- algorithm/social_algorithm/clique_finder.py
+++ algorithm/social_algorithm/clique_finder.py
@@ -26,9 +26,20 @@ e.g. {1: [2, 3], 2: [1, 3], 3: [1, 2]} means student 1 has 'level' or stronger c
 
 
 class CliqueFinder:
+    """
+    Change approach to find all cliques of sizes [1...k] at once and store them in a hash table
+    """
+    cliques = {
+        # size: [[Student]]
+    }
+
     def __init__(self, students: [Student], social_graph: SocialGraph):
         self._students = students
         self.social_graph = social_graph
+        self.cliques = {
+            # a clique of size 1 is simply just the student
+            1: [{student.id} for student in self._students]
+        }
 
     def is_clique(self, subgraph: dict) -> bool:
         # essentially check if this graph is complete
@@ -40,45 +51,35 @@ class CliqueFinder:
         return True
 
     def get_cliques(self, size: int) -> [[int]]:
-        cliques = self.find_cliques_of_size(size)
-        return self.clean_cliques(cliques)
+        if size in self.cliques:
+            return self.cliques[size]
+
+        for k, cliques in self.find_cliques_of_size(size):
+            self.cliques[k] = cliques
+            if k == size:
+                return cliques
 
     def find_cliques_lte_size(self, size: int) -> [[int]]:
         all_cliques = []
         k = size
         while k > 0:
-            all_cliques += self.find_cliques_of_size(k)
+            all_cliques += self.get_cliques(k)
             k -= 1
         return all_cliques
 
     def find_cliques_of_size(self, size: int) -> [[int]]:
-        if size == 1:
-            return [[student.id] for student in self._students]
-
-        # TODO: better algorithm? This one is O(n-choose-k)
-        cliques = []  # [[int]]
-        for members in combinations(self._students, size):
-            member_ids = [student.id for student in members]
-            subgraph = self.social_graph.subgraph(member_ids)
-            if self.is_clique(subgraph):
-                cliques.append(member_ids)
-        return cliques
-
-    def clean_cliques(self, cliques: [[int]]) -> [[int]]:
-        """Clean means no conflicts. A student in one clique cannot be in another, but the normal algorithm doesn't
-        enforce this restriction """
-
-        """
-        cliques = [
-            [1, 2, 3]
-            [3, 4, 5]
-        ]
-        """
-        already_members = []
-        cleaned_cliques = []
-        for clique in cliques:
-            if any([member in already_members for member in clique]):
-                continue
-            already_members += clique
-            cleaned_cliques.append(clique)
-        return cleaned_cliques
+        # Adapted from https://iq.opengenus.org/algorithm-to-find-cliques-of-a-given-size-k/
+        k = 2
+        cliques = [{from_id, to_id} for from_id, to_id in self.social_graph.edges() if from_id != to_id]
+        while cliques:
+            # result
+            yield k, cliques
+            # merge k-cliques into (k+1)-cliques
+            cliques_1 = set()
+            for u, v in combinations(cliques, 2):
+                w = u ^ v
+                if len(w) == 2 and self.social_graph.has_edge(*w):
+                    cliques_1.add(tuple(u | w))
+            # remove duplicates
+            cliques = list(map(set, cliques_1))
+            k += 1
