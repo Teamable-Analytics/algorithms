b0e4bc1edc882f26077d03f202d93093a0e2d778
opeyem1a <opeyadeyemi@gmail.com>
2022-04-07 16:53:26 -0700

    + test file runs and pickles logger classes

--- test.py
+++ test.py
@@ -1,9 +1,19 @@
+import pickle
+import time
+from math import floor
+
 from test_social.encoder import DATA_FILE_PATH
 from test_social.logger import Logger
 from test_social.mock_team_generation import mock_generation
 
 if __name__ == '__main__':
-    logger = Logger(real=True)
-    teams = mock_generation(logger, 5, DATA_FILE_PATH)
-    logger.print_teams(teams, with_friends=True)
+    for _ in range(10, 55, 5):
+        logger = Logger(real=True)
+        teams = mock_generation(logger, 55, DATA_FILE_PATH)
+        logger.end()
+        logger.print_teams(teams, with_friends=True)
+
+        timestamp = floor(time.time())
+        with open(f'test_social/logs/log_{timestamp}.pkl', 'wb') as f:
+            pickle.dump(logger, f)
     a = 1
