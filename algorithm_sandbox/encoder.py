e47348af3acc7c84ee6167c6cc18d7dbdfe58997
opeyem1a <opeyadeyemi@gmail.com>
2022-04-10 01:57:32 -0700

    + improve and document Encoder class

--- algorithm_sandbox/encoder.py
+++ algorithm_sandbox/encoder.py
@@ -1,25 +1,41 @@
+from typing import Dict
+
 from team_formation.app.team_generator.student import Student
 
+
+# Currently hardcoded, these file paths must be from the root directory and lead to the mock data .json files being
+# used.
+
+# Both files are the output from running 'serialize_group_set <canvas_course_id> <gen_group_set_id>' in the main
+# website's Django terminal. The format can be found in the GroupDataRetrieval class in the main repo, too.
 DATA_FILE_PATH = "algorithm_sandbox/mock_data/gen_group_set-20220331_141058.json"
 KEY_FILE_PATH = "algorithm_sandbox/mock_data/student_key-20220331_141058.json"
 
 
 class Encoder:
+    """
+    Responsible for converting anonymized data in mock files into real student names for validation and logging purposes
+    """
     STUDENT_KEY_CACHE = None
 
     @staticmethod
-    def get_student_name(student_anon: Student) -> str:
+    def get_student_name(student: Student) -> str:
         student_key = load_json_data(KEY_FILE_PATH)
-        student_name = Encoder.get_student_name_by_id(student_key, student_anon.id)
+        student_name = Encoder.get_student_name_by_id(student_key, student.id)
         return student_name
 
     @staticmethod
     def get_student_name_by_id(student_key: dict, student_anon_id: int) -> str:
+        """
+        Finds a student's real name given their anonymized id.
+        'No Name (<ID>)' refers to a registered student who's name is null in the database.
+        'Unregistered' refers to an unregistered student
+        """
         for real_id, student_info in student_key.items():
             if student_info['anon_student_id'] == student_anon_id:
                 student_name = student_info['real_name']
                 return student_name if student_name else f'No Name ({real_id})'
-        return f'Unknown Student ({student_anon_id})'
+        return f'Unregistered'
 
     @staticmethod
     def get_student_key():
