99d5b3895f8f1ea3d52951de6fcbb46a776dfece
opeyem1a <opeyadeyemi@gmail.com>
2022-04-10 01:43:20 -0700

    + document algorithm state class

--- algorithm_sandbox/algorithm_state.py
+++ algorithm_sandbox/algorithm_state.py
@@ -5,6 +5,11 @@ from team_formation.app.team_generator.team import Team
 
 
 class AlgorithmState:
+    """
+    This object represents a snapshot of an algorithms run.
+    It contains the current team compositions and the stage of the algorithm.
+    """
+
     def __init__(self, teams: List[Team], stage: int = None):
         self.current_team_compositions = copy.deepcopy(teams)
         self.stage = stage
