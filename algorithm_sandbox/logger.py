d29975e10c77f09bd276da22af66b2eeed8eddec
opeyem1a <opeyadeyemi@gmail.com>
2022-04-10 01:03:11 -0700

    + better logging + only log friends and enemies if they exist and are unsatisfied (this is optional)

--- algorithm_sandbox/logger.py
+++ algorithm_sandbox/logger.py
@@ -1,11 +1,13 @@
 import time
 from typing import List
 
-from team_formation.app.team_generator.algorithm.algorithms import Algorithm
-from team_formation.app.team_generator.student import Student
-from team_formation.app.team_generator.team import Team
 from algorithm_sandbox.algorithm_state import AlgorithmState
 from algorithm_sandbox.encoder import Encoder
+from algorithm_sandbox.evaluation import team_satisfaction_score, team_set_satisfaction_score
+from team_formation.app.team_generator.algorithm.algorithms import Algorithm
+from team_formation.app.team_generator.algorithm.consts import ENEMY, UNREGISTERED_STUDENT_ID, FRIEND
+from team_formation.app.team_generator.student import Student
+from team_formation.app.team_generator.team import Team
 
 
 class Logger:
@@ -19,19 +21,53 @@ class Logger:
         non_empty_teams = [team for team in teams if team.size > 0]
         self.algorithm_states.append(AlgorithmState(non_empty_teams, algorithm.stage))
 
-    def print_teams(self, teams: List[Team], with_friends: bool = False):
+    def print_teams(self, teams: List[Team], with_relationships: bool = False, only_unmet: bool = False):
         print(f'Number of teams: {len(teams)}')
+        print(f'\tOverall Friend Satisfaction Score: {team_set_satisfaction_score(teams, friend=True)}')
+        print(f'\tOverall Enemy Satisfaction Score: {team_set_satisfaction_score(teams, friend=False)}')
         for team in teams:
-            print(f'Team: {team.name} ({team.id})')
+            friend_score = team_satisfaction_score(team, friend=True)
+            enemy_score = team_satisfaction_score(team, friend=False)
+            print(f'Team: {team.name} ({team.id})\t| Friend Score: {friend_score}\t| Enemy Score: {enemy_score}')
             for student in team.students:
-                info = student.id
-                if self.real:
-                    info = Encoder.get_student_from_key(student, with_friends)
-                print(f'\tStudent - {info}')
+                print(f'\t{self.format_student(student, with_relationships, team if only_unmet else None)}')
+
+    def format_student(self, student: Student, with_relationships: bool = False, team: Team = None) -> str:
+        if not self.real:
+            return f'Student - {student.id}'
+        team_member_ids = [student.id for student in team.students] if team else []
+        student_name = Encoder.get_student_name(student)
+        student_key = Encoder.get_student_key()
+
+        friends = [
+            Encoder.get_student_name_by_id(student_key, other_id)
+            for other_id, relationship
+            in student.relationships.items()
+            if relationship == FRIEND and other_id != UNREGISTERED_STUDENT_ID and
+               (other_id in team_member_ids if team else True)
+        ]
+
+        enemies = [
+            Encoder.get_student_name_by_id(student_key, other_id)
+            for other_id, relationship
+            in student.relationships.items()
+            if relationship == ENEMY and other_id != UNREGISTERED_STUDENT_ID and
+               (other_id in team_member_ids if team else True)
+        ]
+
+        output = f'{student_name}'
+        if not with_relationships:
+            return output
+
+        if friends:
+            output += f' => {"Missing" if team else ""} Friends ({", ".join(friends)})'
+        if enemies:
+            output += f' => {"Included" if team else ""} Enemies ({", ".join(enemies)})'
+        return output
 
     def format_clique(self, clique: List[Student]):
         if self.real:
-            return [*map(Encoder.get_student_from_key, clique)]
+            return [*map(self.format_student, clique)]
         return [student.id for student in clique]
 
     def print_clique(self, clique: List[Student]):
