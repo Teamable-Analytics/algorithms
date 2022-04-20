c6941415ff8296bb6e7cf157a7ccab242569b26d
opeyem1a <opeyadeyemi@gmail.com>
2022-04-20 02:32:49 -0700

    + increase size of visualization

--- algorithm_sandbox/visualization/visualize_teams.py
+++ algorithm_sandbox/visualization/visualize_teams.py
@@ -10,7 +10,7 @@ def visualize_teams_network(visualize_logs: VisualizeLogs):
     app.layout = html.Div([
         visdcc.Network(id='net',
                        options=dict(
-                           height='500px',
+                           height='650px',
                            width='100%',
                            edges={'arrows': {'to': {'enabled': True}}},
                            physics={'barnesHut': {'avoidOverlap': 0.1}},
