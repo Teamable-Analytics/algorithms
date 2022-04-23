6cb76d35328d3dd8aa257bca1e5321fe561b3cae
opeyem1a <opeyadeyemi@gmail.com>
2022-04-22 21:28:33 -0700

    + better test case

--- test_priority.py
+++ test_priority.py
@@ -11,14 +11,23 @@ if __name__ == '__main__':
         priorities=[
             {
                 'order': 1,
+                'constraint': Priority.TYPE_CONCENTRATE,
+                'skill_id': 73,  # timeslot availability
+                'limit_option': Priority.MAX_OF,
+                'limit': 2,
+                'value': 3,  # some timeslot
+            },
+            {
+                'order': 1,
                 'constraint': Priority.TYPE_DIVERSIFY,
-                'skill_id': 73,
+                'skill_id': 81,  # gender
                 'limit_option': Priority.MIN_OF,
                 'limit': 2,
-                'value': 3,
+                'value': 2,  # female
             }
         ],
-        diversify_options=[{'id': 73}]
+        diversify_options=[{'id': 81}],
+        concentrate_options=[{'id': 73}]
     )
     teams = mock_generation(PriorityAlgorithm, algorithm_options, logger, 56, DATA_FILE_PATH)
     logger.end()
