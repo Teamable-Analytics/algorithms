b56425c1435ce27e9891786e7c14af264fe7f880
Keyvan <keyvan@tribe.so>
2022-11-21 16:05:24 -0800

    add gini index

--- algorithm_sandbox/metrics/priority_metrics.py
+++ algorithm_sandbox/metrics/priority_metrics.py
@@ -9,8 +9,8 @@ def is_priority_satisfied(team: Team, priority):
     for student in students:
         value = student.get_skill(skill_id)[0]
         if value not in cnt:
-            cnt['value'] = 0
-        cnt['value'] += 1
+            cnt[value] = 0
+        cnt[value] += 1
 
     for value in cnt.values():
         if priority['limit_option'] == Priority.MIN_OF:
@@ -22,11 +22,30 @@ def is_priority_satisfied(team: Team, priority):
     return True
 
 
+def gini_index(team: Team, priority):
+    students = team.students
+    skill_id = priority['skill_id']
+    cnt = {}
+    for student in students:
+        value = student.get_skill(skill_id)[0]
+        if value not in cnt:
+            cnt[value] = 0
+        cnt[value] += 1
+
+    gini = 0
+    for value in cnt.values():
+        gini += value * (value - 1)
+
+    gini = gini / (len(students) * (len(students) - 1))
+    return 1 - gini
+
+
 def get_priority_metrics(teams: list[Team], priorities):
     metrics = {
         'EXP': 0,
         'LN': 0,
         'MAX': 0,
+        'AVG_GINI': 0,
     }
 
     k = len(priorities)
@@ -39,8 +58,9 @@ def get_priority_metrics(teams: list[Team], priorities):
             if not sat:
                 total_sat = False
 
-            metrics['LN'] += sat * (k - i) / (k * (k+1)/2) / m
-            metrics['EXP'] += sat * (2**(k-i-1)) / (2**k - 1) / m
+            metrics['LN'] += sat * (k - i) / (k * (k + 1) / 2) / m
+            metrics['EXP'] += sat * (2 ** (k - i - 1)) / (2 ** k - 1) / m
+            metrics['AVG_GINI'] += gini_index(team, priority) / m / k
         if total_sat:
             metrics['MAX'] = i + 1
 
