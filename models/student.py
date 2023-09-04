6d57e4b14205b52c968f1634c1ac6d5c908f0a4e
ketphan02 <tuankietvn@outlook.com>
2023-09-04 16:11:52 -0700

    Avoid circular imports

--- models/student.py
+++ models/student.py
@@ -2,7 +2,6 @@ from dataclasses import dataclass, field
 from typing import List, Dict
 
 from models.enums import Relationship
-from models.team import Team
 
 
 @dataclass
@@ -12,7 +11,7 @@ class Student:
     attributes: Dict[int, List[any]] = field(default_factory=dict)
     relationships: Dict[int, Relationship] = field(default_factory=dict)
     preferences: List[int] = field(default_factory=list)
-    team: Team = None
+    team: "Team" = None
 
     def get_id(self):
         return self._id
