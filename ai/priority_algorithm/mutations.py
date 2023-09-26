571a7b6a6a670f696841f6eb4029d06d8410d2f5
justino599 <justin.schoenit@gmail.com>
2023-09-25 18:48:49 -0700

    Get best combination

--- ai/priority_algorithm/mutations.py
+++ ai/priority_algorithm/mutations.py
@@ -46,15 +46,17 @@ def mutate_robinhood(
     Robinhood is a mutation that finds a team t1 that does not satisfy a priority c, and a team t2 that does satisfy c.
     It then creates all possible teams using the students of t1 and t2, and chooses the M best teams.
     """
-    available_priority_teams = [
-        priority_team
-        for priority_team in priority_team_set.priority_teams
-        if not priority_team.team.is_locked
-    ]
+    best_team_set: PriorityTeamSet = priority_team_set
+    best_team_set_score: float = priority_team_set.calculate_score(priorities, student_dict)
     try:
-        best_team_set: PriorityTeamSet = None
-        best_team_set_score: int = 0
         for priority in priorities:
+            cloned_priority_team_set: PriorityTeamSet = priority_team_set.clone()
+            available_priority_teams = [
+                priority_team
+                for priority_team in cloned_priority_team_set.priority_teams
+                if not priority_team.team.is_locked
+            ]
+
             # Get all teams that satisfy the priority
             satisfied_teams: List[PriorityTeam] = [
                 team for team in available_priority_teams if priority.satisfied_by(
@@ -68,7 +70,7 @@ def mutate_robinhood(
 
             # Choose a random team from each list
             satisfied_team: PriorityTeam = satisfied_teams.pop(random.randrange(len(satisfied_teams)))
-            unsatisfied_team: PriorityTeam = random.choice(unsatisfied_teams)
+            unsatisfied_team: PriorityTeam = unsatisfied_teams.pop(random.randrange(len(unsatisfied_teams)))
 
             # List of all students in the two teams
             students: List[int] = satisfied_team.student_ids + unsatisfied_team.student_ids
@@ -85,13 +87,23 @@ def mutate_robinhood(
                 team_2 = satisfied_team
                 team_2.student_ids = [student_id for student_id in students if student_id not in team]
 
-                # Keep the best teamset only
+                # Build the resulting PriorityTeamSet
+                cloned_priority_team_set.priority_teams = [
+                    *[
+                        team for team in cloned_priority_team_set.priority_teams if team not in [team_1, team_2]
+                    ],
+                    team_1,
+                    team_2
+                ]
 
-
-
+                # Calculate the score of the resulting PriorityTeamSet
+                score = cloned_priority_team_set.calculate_score(priorities, student_dict)
+                if score > best_team_set_score:
+                    best_team_set = cloned_priority_team_set
+                    best_team_set_score = score
     except ValueError:
-        return priority_team_set
-    return priority_team_set
+        return best_team_set
+    return best_team_set
 
 
 def score(
@@ -107,7 +119,7 @@ def score(
         [
             satisfaction * multiplier
             for satisfaction, multiplier in zip(
-                priority_satisfaction_array, multipliers
-            )
+            priority_satisfaction_array, multipliers
+        )
         ]
     )
