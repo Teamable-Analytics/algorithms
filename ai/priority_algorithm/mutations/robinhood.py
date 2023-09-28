7b90677cad98ab73db9507bbf1266c918201dbec
justino599 <justin.schoenit@gmail.com>
2023-09-27 17:49:01 -0700

    Implement and test robinhood variation

--- ai/priority_algorithm/mutations/robinhood.py
+++ ai/priority_algorithm/mutations/robinhood.py
@@ -1,8 +1,9 @@
 import itertools
 import random
-from typing import List, Dict
+from typing import List, Dict, Tuple
 
 from ai.priority_algorithm.interfaces import Priority
+from ai.priority_algorithm.mutations import utils
 from ai.priority_algorithm.priority_teamset import PriorityTeamSet, PriorityTeam
 from models.student import Student
 
@@ -82,13 +83,52 @@ def mutate_robinhood(
     return best_team_set
 
 
+def mutate_robinhood_holistic(
+    priority_team_set: PriorityTeamSet,
+    priorities: List[Priority],
+    student_dict: Dict[int, Student],
+) -> PriorityTeamSet:
+    """
+    This is a variation of mutate_robinhood that does not consider individual priorities. Instead, it considers the entire set of priorities as a whole. This is done by calculating the score of each team in the team set, and then performing the local max portion of the robinhood mutation on the team with the lowest score and the team with the highest score.
+    """
+
+    if not valid_robinhood_arguments(priority_team_set, priorities, student_dict):
+        return priority_team_set
+
+    available_priority_teams = [
+        priority_team
+        for priority_team in priority_team_set.priority_teams
+        if not priority_team.team.is_locked
+    ]
+
+    # Calculate the score of each team in the team set
+    team_scores: List[Tuple[PriorityTeam, int]] = []
+    for team in available_priority_teams:
+        team_scores.append((team, utils.score(team, priorities, student_dict)))
+
+    # Find the min and max scores
+    min_scoring_team: Tuple[PriorityTeam, int] = min(team_scores, key=lambda x: x[1])
+    max_scoring_team: Tuple[PriorityTeam, int] = max(team_scores, key=lambda x: x[1])
+
+    # Perform local max portion of robinhood
+    team_set, score = perform_local_max_portion_of_robinhood(
+        priority_team_set,
+        priorities,
+        student_dict,
+        min_scoring_team[0],
+        max_scoring_team[0],
+    )
+
+    return team_set
+
+
 def perform_local_max_portion_of_robinhood(
     cloned_priority_team_set: PriorityTeamSet,
     priorities: List[Priority],
     student_dict: Dict[int, Student],
     selected_team_a: PriorityTeam,
     selected_team_b: PriorityTeam,
-) -> (PriorityTeamSet, float):
+) -> Tuple[PriorityTeamSet, float]:
     """
     Performs the local max portion of the robinhood mutation. This is the part where we generate all possible teams using the students from the two teams, and choose the best team.
 
