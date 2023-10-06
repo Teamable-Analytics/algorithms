c6970d9c4e8abc9c2e2ac88765753898c8351ed0
Hedgemon4 <sethlinc4@gmail.com>
2023-10-06 09:14:46 -0700

    Renamed directory

--- tests/test_api/test_ai/test_priority_algorithm/test_mutations/test_util.py
+++ tests/test_api/test_ai/test_priority_algorithm/test_mutations/test_util.py
@@ -6,7 +6,7 @@ from api.ai.priority_algorithm.mutations.utils import score
 from api.ai.priority_algorithm.priority_teamset import PriorityTeamSet, PriorityTeam
 from api.models.student import Student
 from api.models.team import Team
-from tests.test_ai.test_priority_algorithm.test_mutations.test_local_max import (
+from tests.test_api.test_ai.test_priority_algorithm.test_mutations.test_local_max import (
     EvenPriority,
 )
 
