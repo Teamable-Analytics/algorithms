5682d1eecac1c811fedf04185d834bbd3f9c1987
Hedgemon4 <sethlinc4@gmail.com>
2023-10-06 09:38:23 -0700

    Removed unused part of the test

--- tests/test_benchmarking/test_simulation/test_goal_to_priority.py
+++ tests/test_benchmarking/test_simulation/test_goal_to_priority.py
@@ -16,40 +16,6 @@ from benchmarking.simulation.goal_to_priority import (
 )
 
 
-class TestScenario(Scenario):
-    def __init__(self, value_of_gpa: int, value_of_age: int):
-        self.value_of_gpa = value_of_gpa
-        self.value_of_age = value_of_age
-
-    @property
-    def name(self) -> str:
-        return "Test Scenario"
-
-    @property
-    def goals(self) -> List[Goal]:
-        return [
-            DiversityGoal(
-                DiversifyType.CONCENTRATE,
-                ScenarioAttribute.GPA.value,
-                tokenization_constraint=TokenizationConstraint(
-                    direction=TokenizationConstraintDirection.MIN_OF,
-                    threshold=2,
-                    value=self.value_of_gpa,
-                ),
-            ),
-            DiversityGoal(
-                DiversifyType.DIVERSIFY,
-                ScenarioAttribute.AGE.value,
-                tokenization_constraint=TokenizationConstraint(
-                    direction=TokenizationConstraintDirection.MAX_OF,
-                    threshold=2,
-                    value=self.value_of_age,
-                ),
-            ),
-            WeightGoal(diversity_goal_weight=1),
-        ]
-
-
 class TestGoalToPriority(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
