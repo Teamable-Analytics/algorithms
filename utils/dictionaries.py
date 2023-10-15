2436824e85c9f923119acdf5bc855797b6689a70
opeyem1a <opeyadeyemi@gmail.com>
2023-10-15 03:51:41 -0600

    lint

--- utils/dictionaries.py
+++ utils/dictionaries.py
@@ -4,6 +4,4 @@ from typing import Dict, Any, List
 def prune_dictionary_keys(
     dictionary: Dict[str, Any], keys_to_keep: List[str]
 ) -> Dict[str, Any]:
-    return {
-        key: value for key, value in dictionary.items() if key in keys_to_keep
-    }
+    return {key: value for key, value in dictionary.items() if key in keys_to_keep}
