7cc87511ce8f6fffdfac244ea925c6746c78e031
Hedgemon4 <sethlinc4@gmail.com>
2023-10-21 10:23:18 -0700

    implemented serializer

--- api/models/student/student_serializer.py
+++ api/models/student/student_serializer.py
@@ -1,17 +1,33 @@
 import json
+import string
 
+from api.models.enums import Relationship
 from api.models.student.student import Student
 
 
 class StudentSerializer(json.JSONEncoder, json.JSONDecoder):
-    def default(self, student):
-        print(type(student))
+    def default(self, student: Student) -> string:
         if not isinstance(student, Student):
             raise TypeError("Object is not a student instance.")
         return {
             "_id": student._id,
             "name": student.name,
             "attributes": student.attributes,
-            "relationships": {f"{x}": y.value for x, y in student.relationships.items()},
-            "project_preferences": student.project_preferences
+            "relationships": {
+                f"{x}": y.value for x, y in student.relationships.items()
+            },
+            "project_preferences": student.project_preferences,
         }
+
+    def decode(self, json_string, _w=...) -> Student:
+        data = json.loads(json_string)
+        return Student(
+            _id=data["_id"],
+            name=data["name"],
+            attributes=data["attributes"],
+            relationships={
+                int(key): Relationship(value)
+                for key, value in data["relationships"].items()
+            },
+            project_preferences=data["project_preferences"],
+        )
