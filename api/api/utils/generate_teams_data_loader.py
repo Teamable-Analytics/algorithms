76b58d3b092e891d4fb500eb890b024357c9de24
ketphan02 <tuankietvn@outlook.com>
2023-10-20 23:01:43 -0700

    Resolve comments

--- api/api/utils/generate_teams_data_loader.py
+++ api/api/utils/generate_teams_data_loader.py
@@ -7,6 +7,7 @@ from api.ai.new.interfaces.team_generation_options import TeamGenerationOptions
 from api.models.enums import AlgorithmType, Relationship
 from api.models.student import Student
 from api.api.utils.relationship import get_relationship
+from api.models.team import TeamShell
 
 
 @dataclass
@@ -62,7 +63,7 @@ class GenerateTeamsDataLoader:
 
         return TeamGenerationOptions(
             total_teams=team_generation_options.get("total_teams"),
-            initial_teams=team_generation_options.get("initial_teams"),
+            initial_teams=[TeamShell(team) for team in team_generation_options.get("initial_teams")],
             max_team_size=team_generation_options.get("max_team_size"),
             min_team_size=team_generation_options.get("min_team_size"),
         )
