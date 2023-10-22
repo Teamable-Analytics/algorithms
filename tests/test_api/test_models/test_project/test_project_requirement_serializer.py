c5c6adc134e2817816f97195ad84f642328294a1
Hedgemon4 <sethlinc4@gmail.com>
2023-10-22 11:06:47 -0700

    Finished unit tests

--- tests/test_api/test_models/test_project/test_project_requirement_serializer.py
+++ tests/test_api/test_models/test_project/test_project_requirement_serializer.py
@@ -1,3 +1,4 @@
+import json
 import unittest
 
 from api.models.enums import RequirementOperator
@@ -18,9 +19,28 @@ class TestProjectRequirementSerializer(unittest.TestCase):
         ]
         cls.json_project_requirements = [
             '{"attribute": 4, "operator": "exactly", "value": 5}',
-            '{"attribute": 1, "operator": "less than", "value": 2}'
+            '{"attribute": 1, "operator": "less than", "value": 2}',
         ]
 
-    def test_project_requirement_serializer__encode_returns_dict(self):
-        encoder = ProjectRequirementSerializer()
-        project_requirement_encoded = encoder.encode()
+    def test_project_requirement_serializer__encodes_project_requirement_correctly(
+        self,
+    ):
+        encoded_project_requirements = [
+            json.dumps(pr, cls=ProjectRequirementSerializer)
+            for pr in self.project_requirements
+        ]
+        self.assertEqual(self.json_project_requirements, encoded_project_requirements)
+
+    def test_project_requirement_serializer__decode_returns_project_requirement(self):
+        decoder = ProjectRequirementSerializer()
+        decoded_project_requirement = decoder.decode(self.json_project_requirements[0])
+        self.assertIsInstance(decoded_project_requirement, cls=ProjectRequirement)
+
+    def test_project_requirement_serializer__decodes_project_requirements_correctly(
+        self,
+    ):
+        decoder = ProjectRequirementSerializer()
+        decoded_project_requirements = [
+            decoder.decode(pr) for pr in self.json_project_requirements
+        ]
+        self.assertEqual(self.project_requirements, decoded_project_requirements)
