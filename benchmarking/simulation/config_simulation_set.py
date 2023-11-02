cfa594449c82609aa05cccba3df19145417aaba6
Hedgemon4 <sethlinc4@gmail.com>
2023-11-01 20:50:48 -0700

    Finished implementing config simulation set

--- benchmarking/simulation/config_simulation_set.py
+++ benchmarking/simulation/config_simulation_set.py
@@ -1,8 +1,12 @@
-from typing import List
+from typing import List, Dict
 
 from api.ai.new.interfaces.algorithm_config import AlgorithmConfig
 from api.models.enums import AlgorithmType
+from benchmarking.simulation.simulation import SimulationArtifact, Simulation
 from benchmarking.simulation.simulation_settings import SimulationSettings
+from utils.validation import is_unique
+
+ConfigSimulationSetArtifact = Dict[str, SimulationArtifact]
 
 
 class ConfigSimulationSet:
@@ -10,9 +14,48 @@ class ConfigSimulationSet:
     Represents a set of Simulation runs for when you want to run 1 algorithm with multiple configs
     """
 
-    def __init__(self, settings: SimulationSettings, algorithm_type: AlgorithmType,
-                 algorithm_configs: List[AlgorithmConfig]):
+    def __init__(
+        self,
+        settings: SimulationSettings,
+        algorithm_type: AlgorithmType,
+        algorithm_configs: List[AlgorithmConfig],
+    ):
         self.algorithm_type = algorithm_type
         self.base_settings = settings
         self.base_cache_key = self.base_settings.cache_key
+        names = [_.name for _ in algorithm_configs]
+        if any(item is None for item in names) or not is_unique(names):
+            raise ValueError("Each algorithm config must have a unique name")
         self.algorithm_configs = algorithm_configs
+        self.basic_simulation_set_artifact: ConfigSimulationSetArtifact = {}
+
+    def run(self, num_runs: int) -> ConfigSimulationSetArtifact:
+        for config in self.algorithm_configs:
+            self.basic_simulation_set_artifact[config.name] = Simulation(
+                algorithm_type=self.algorithm_type,
+                settings=self.get_simulation_settings_from_base(name=config.name),
+                config=config,
+            ).run(num_runs)
+
+        return self.basic_simulation_set_artifact
+
+    def get_simulation_settings_from_base(self, name: str) -> SimulationSettings:
+        cache_key = (
+            f"{self.base_settings.cache_key}/{name}"
+            if self.base_settings.cache_key
+            else None
+        )
+        return SimulationSettings(
+            scenario=self.base_settings.scenario,
+            student_provider=self.base_settings.student_provider,
+            initial_teams_provider=self.base_settings.initial_teams_provider,
+            num_teams=self.base_settings.num_teams,
+            cache_key=cache_key,
+        )
+
+    @staticmethod
+    def get_artifact(
+        config_simulation_set_artifact: ConfigSimulationSetArtifact,
+        name: str,
+    ) -> SimulationArtifact:
+        return config_simulation_set_artifact.get(name, ([], []))
---
+++ tests/test_benchmarking/test_simulation/test_config_simulation_set.py
@@ -0,0 +1,104 @@
+import unittest
+
+from api.ai.new.interfaces.algorithm_config import (
+    RandomAlgorithmConfig,
+    SocialAlgorithmConfig,
+    PriorityAlgorithmConfig,
+)
+from api.models.enums import AlgorithmType
+from benchmarking.data.simulated_data.mock_student_provider import (
+    MockStudentProvider,
+    MockStudentProviderSettings,
+)
+from benchmarking.simulation.config_simulation_set import ConfigSimulationSet
+from benchmarking.simulation.simulation_settings import SimulationSettings
+from tests.test_benchmarking.test_simulation._data import TestScenario
+from utils.validation import is_unique
+
+
+class TestConfigSimulationSet(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.scenario = TestScenario()
+        cls.student_provider = MockStudentProvider(
+            MockStudentProviderSettings(number_of_students=10),
+        )
+
+    def test_init__makes_sure_keys_not_none(self):
+        base_settings = SimulationSettings(
+            num_teams=2,
+            scenario=self.scenario,
+            student_provider=self.student_provider,
+            cache_key="test_cache_key",
+        )
+        with self.assertRaises(ValueError):
+            ConfigSimulationSet(
+                settings=base_settings,
+                algorithm_type=AlgorithmType.RANDOM,
+                algorithm_configs=[
+                    RandomAlgorithmConfig(name="Steve"),
+                    RandomAlgorithmConfig(),
+                ],
+            ),
+
+    def test_init__makes_sure_names_are_unique(self):
+        base_settings = SimulationSettings(
+            num_teams=2,
+            scenario=self.scenario,
+            student_provider=self.student_provider,
+            cache_key="test_cache_key",
+        )
+        with self.assertRaises(ValueError):
+            ConfigSimulationSet(
+                settings=base_settings,
+                algorithm_type=AlgorithmType.SOCIAL,
+                algorithm_configs=[
+                    SocialAlgorithmConfig(name="Steve"),
+                    SocialAlgorithmConfig(name="Name"),
+                    SocialAlgorithmConfig(name="Steve"),
+                ],
+            ),
+
+    def test_get_simulation_settings_from_base__preserves_all_other_settings(self):
+        # ensures that if more fields are added to the settings class,
+        #   then they are updated in this method to ensure preservation
+        base_settings = SimulationSettings(
+            num_teams=2,
+            scenario=self.scenario,
+            student_provider=self.student_provider,
+            cache_key="test_cache_key",
+        )
+        simulation_set = ConfigSimulationSet(
+            settings=base_settings,
+            algorithm_type=AlgorithmType.RANDOM,
+            algorithm_configs=[RandomAlgorithmConfig("another")],
+        )
+        modified_settings = simulation_set.get_simulation_settings_from_base("another")
+        self.assertIsNotNone(base_settings.cache_key)
+        for field_name in SimulationSettings.__dataclass_fields__.keys():
+            if field_name == "cache_key":  # cache key should be different
+                continue
+            self.assertEqual(
+                modified_settings.__getattribute__(field_name),
+                base_settings.__getattribute__(field_name),
+            )
+
+    def test_get_simulation_settings_from_base__creates_unique_cache_keys(self):
+        algorithm_configs = [RandomAlgorithmConfig("another"), RandomAlgorithmConfig("one")]
+        simulation_set = ConfigSimulationSet(
+            settings=SimulationSettings(
+                num_teams=2,
+                scenario=self.scenario,
+                student_provider=self.student_provider,
+                cache_key="test_cache_key",
+            ),
+            algorithm_type=AlgorithmType.RANDOM,
+            algorithm_configs=algorithm_configs,
+        )
+
+        generated_keys = [
+            simulation_set.get_simulation_settings_from_base(config.name).cache_key
+            for config in algorithm_configs
+        ]
+        self.assertGreater(len(generated_keys), 0)
+        self.assertTrue(is_unique(generated_keys))
