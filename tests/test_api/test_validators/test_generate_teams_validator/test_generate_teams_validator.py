2b4a2d1efc48fe2f78ac5353591ed69e533fce2f
ketphan02 <tuankietvn@outlook.com>
2023-11-06 22:38:50 -0800

    Initial team not always there lol

--- tests/test_api/test_validators/test_generate_teams_validator/test_generate_teams_validator.py
+++ tests/test_api/test_validators/test_generate_teams_validator/test_generate_teams_validator.py
@@ -3,7 +3,7 @@ import copy
 
 from schema import SchemaError
 
-from validators.generate_teams_validator import GenerateTeamsValidator
+from api.api.validators.generate_teams_validator import GenerateTeamsValidator
 
 
 class TestGenerateTeamsValidator(unittest.TestCase):
