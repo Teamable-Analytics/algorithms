0eeff94b8f1b5fad895998f971997c9aac00741d
Opey Adeyemi <60050089+Opeyem1a@users.noreply.github.com>
2023-11-12 14:57:10 +0700

    switch runs to class structure (#217)

--- benchmarking/runs/diversify_gender_min_2.py
+++ benchmarking/runs/diversify_gender_min_2.py
@@ -32,174 +32,183 @@ from benchmarking.evaluations.metrics.priority_satisfaction import PrioritySatis
 from benchmarking.evaluations.scenarios.diversify_gender_min_2_female import (
     DiversifyGenderMin2Female,
 )
+from benchmarking.runs.interfaces import Run
 from benchmarking.simulation.goal_to_priority import goals_to_priorities
 from benchmarking.simulation.insight import Insight
 from benchmarking.simulation.simulation_set import SimulationSet, SimulationSetArtifact
 from benchmarking.simulation.simulation_settings import SimulationSettings
 
 
-def diversify_gender_min_2(num_trials: int = 10, generate_graphs: bool = True):
-    """
-    Goal: Run diversify gender scenario, measure average, min, and max gini index
-    """
+class DiversifyGenderMin2Run(Run):
+    @staticmethod
+    def start(num_trials: int = 10, generate_graphs: bool = True):
+        """
+        Goal: Run diversify gender scenario, measure average, min, and max gini index
+        """
 
-    # Defining our changing x-values (in the graph sense)
-    class_sizes = list(range(50, 501, 50))
-    ratio_of_female_students = 0.2
+        # Defining our changing x-values (in the graph sense)
+        class_sizes = list(range(50, 501, 50))
+        ratio_of_female_students = 0.2
 
-    scenario = DiversifyGenderMin2Female(value_of_female=Gender.FEMALE.value)
+        scenario = DiversifyGenderMin2Female(value_of_female=Gender.FEMALE.value)
 
-    graph_runtime_dict = {}
-    graph_avg_gini_dict = {}
-    graph_min_gini_dict = {}
-    graph_max_gini_dict = {}
-    graph_priority_dict = {}
-    graph_dicts = [
-        graph_runtime_dict,
-        graph_avg_gini_dict,
-        graph_min_gini_dict,
-        graph_max_gini_dict,
-        graph_priority_dict,
-    ]
+        graph_runtime_dict = {}
+        graph_avg_gini_dict = {}
+        graph_min_gini_dict = {}
+        graph_max_gini_dict = {}
+        graph_priority_dict = {}
+        graph_dicts = [
+            graph_runtime_dict,
+            graph_avg_gini_dict,
+            graph_min_gini_dict,
+            graph_max_gini_dict,
+            graph_priority_dict,
+        ]
 
-    metrics: Dict[str, TeamSetMetric] = {
-        "AverageGiniIndex": AverageGiniIndex(attribute=ScenarioAttribute.GENDER.value),
-        "MaxGiniIndex": MaximumGiniIndex(attribute=ScenarioAttribute.GENDER.value),
-        "MinGiniIndex": MinimumGiniIndex(attribute=ScenarioAttribute.GENDER.value),
-        "PrioritySatisfaction": PrioritySatisfaction(
-            goals_to_priorities(
-                [goal for goal in scenario.goals if isinstance(goal, DiversityGoal)]
+        metrics: Dict[str, TeamSetMetric] = {
+            "AverageGiniIndex": AverageGiniIndex(
+                attribute=ScenarioAttribute.GENDER.value
             ),
-            False,
-        ),
-    }
-
-    artifacts: Dict[int, SimulationSetArtifact] = {}
-
-    for class_size in class_sizes:
-        print("CLASS SIZE /", class_size)
-
-        number_of_teams = math.ceil(class_size / 5)
-
-        # set up either mock or real data
-        student_provider_settings = MockStudentProviderSettings(
-            number_of_students=class_size,
-            attribute_ranges={
-                ScenarioAttribute.GENDER.value: [
-                    (Gender.MALE, 1 - ratio_of_female_students),
-                    (Gender.FEMALE, ratio_of_female_students),
-                ],
-            },
-        )
-
-        simulation_set_artifact = SimulationSet(
-            settings=SimulationSettings(
-                num_teams=number_of_teams,
-                scenario=scenario,
-                student_provider=MockStudentProvider(student_provider_settings),
-                cache_key=f"diversify_gender_min_2_{number_of_teams}",
-            ),
-            algorithm_set={
-                AlgorithmType.RANDOM: [RandomAlgorithmConfig()],
-                AlgorithmType.SOCIAL: [SocialAlgorithmConfig()],
-                AlgorithmType.WEIGHT: [WeightAlgorithmConfig()],
-                AlgorithmType.PRIORITY: [
-                    PriorityAlgorithmConfig(),
-                    PriorityAlgorithmConfig(
-                        name="local_max",
-                        MUTATIONS=[(mutate_local_max, 1), (mutate_random_swap, 2)],
-                    ),
-                ],
-            },
-        ).run(num_runs=num_trials)
-        artifacts[class_size] = simulation_set_artifact
-
-    if generate_graphs:
-        for class_size, artifact in artifacts.items():
-            insight_set: Dict[str, Dict[str, List[float]]] = Insight.get_output_set(
-                artifact=artifact, metrics=list(metrics.values())
-            )
-
-            average_ginis = Insight.average_metric(insight_set, "AverageGiniIndex")
-            maximum_ginis = Insight.average_metric(insight_set, "MaximumGiniIndex")
-            minimum_ginis = Insight.average_metric(insight_set, "MinimumGiniIndex")
-            average_runtimes = Insight.average_metric(insight_set, Insight.KEY_RUNTIMES)
-            satisfied_priorities = Insight.average_metric(
-                insight_set, "PrioritySatisfaction"
-            )
-
-            metric_values = [
-                average_runtimes,
-                average_ginis,
-                minimum_ginis,
-                maximum_ginis,
-                satisfied_priorities,
-            ]
-
-            # Data processing for graph
-            for i, metric in enumerate(metric_values):
-                for name, data in metric.items():
-                    if name not in graph_dicts[i]:
-                        graph_dicts[i][name] = GraphData(
-                            x_data=[class_size],
-                            y_data=[data],
-                            name=name,
-                        )
-                    else:
-                        graph_dicts[i][name].x_data.append(class_size)
-                        graph_dicts[i][name].y_data.append(data)
-
-        line_graph(
-            LineGraphMetadata(
-                x_label="Class size",
-                y_label="Run time (seconds)",
-                title="Diversify Gender With Min of Two Runtimes",
-                data=list(graph_runtime_dict.values()),
-            )
-        )
-
-        line_graph(
-            LineGraphMetadata(
-                x_label="Class size",
-                y_label="Average Gini Index",
-                title="Diversify Gender With Min of Two Average Gini Index",
-                data=list(graph_avg_gini_dict.values()),
-                y_lim=GraphAxisRange(*metrics["AverageGiniIndex"].theoretical_range),
-            )
-        )
-
-        line_graph(
-            LineGraphMetadata(
-                x_label="Class size",
-                y_label="Minimum Gini Index",
-                title="Diversify Gender With Min of Two Minimum Gini",
-                data=list(graph_min_gini_dict.values()),
-                y_lim=GraphAxisRange(*metrics["MinGiniIndex"].theoretical_range),
-            )
-        )
-
-        line_graph(
-            LineGraphMetadata(
-                x_label="Class size",
-                y_label="Maximum Gini Index",
-                title="Diversify Gender With Min of Two Max Gini",
-                data=list(graph_max_gini_dict.values()),
-                y_lim=GraphAxisRange(*metrics["MaxGiniIndex"].theoretical_range),
-            )
-        )
-
-        line_graph(
-            LineGraphMetadata(
-                x_label="Class size",
-                y_label="Priorities Satisfied",
-                title="Diversity Gender With Min of Two Satisfied Priorities",
-                data=list(graph_priority_dict.values()),
-                y_lim=GraphAxisRange(
-                    *metrics["PrioritySatisfaction"].theoretical_range
+            "MaxGiniIndex": MaximumGiniIndex(attribute=ScenarioAttribute.GENDER.value),
+            "MinGiniIndex": MinimumGiniIndex(attribute=ScenarioAttribute.GENDER.value),
+            "PrioritySatisfaction": PrioritySatisfaction(
+                goals_to_priorities(
+                    [goal for goal in scenario.goals if isinstance(goal, DiversityGoal)]
                 ),
+                False,
+            ),
+        }
+
+        artifacts: Dict[int, SimulationSetArtifact] = {}
+
+        for class_size in class_sizes:
+            print("CLASS SIZE /", class_size)
+
+            number_of_teams = math.ceil(class_size / 5)
+
+            # set up either mock or real data
+            student_provider_settings = MockStudentProviderSettings(
+                number_of_students=class_size,
+                attribute_ranges={
+                    ScenarioAttribute.GENDER.value: [
+                        (Gender.MALE, 1 - ratio_of_female_students),
+                        (Gender.FEMALE, ratio_of_female_students),
+                    ],
+                },
             )
-        )
+
+            simulation_set_artifact = SimulationSet(
+                settings=SimulationSettings(
+                    num_teams=number_of_teams,
+                    scenario=scenario,
+                    student_provider=MockStudentProvider(student_provider_settings),
+                    cache_key=f"diversify_gender_min_2_{number_of_teams}",
+                ),
+                algorithm_set={
+                    AlgorithmType.RANDOM: [RandomAlgorithmConfig()],
+                    AlgorithmType.SOCIAL: [SocialAlgorithmConfig()],
+                    AlgorithmType.WEIGHT: [WeightAlgorithmConfig()],
+                    AlgorithmType.PRIORITY: [
+                        PriorityAlgorithmConfig(),
+                        PriorityAlgorithmConfig(
+                            name="local_max",
+                            MUTATIONS=[(mutate_local_max, 1), (mutate_random_swap, 2)],
+                        ),
+                    ],
+                },
+            ).run(num_runs=num_trials)
+            artifacts[class_size] = simulation_set_artifact
+
+        if generate_graphs:
+            for class_size, artifact in artifacts.items():
+                insight_set: Dict[str, Dict[str, List[float]]] = Insight.get_output_set(
+                    artifact=artifact, metrics=list(metrics.values())
+                )
+
+                average_ginis = Insight.average_metric(insight_set, "AverageGiniIndex")
+                maximum_ginis = Insight.average_metric(insight_set, "MaximumGiniIndex")
+                minimum_ginis = Insight.average_metric(insight_set, "MinimumGiniIndex")
+                average_runtimes = Insight.average_metric(
+                    insight_set, Insight.KEY_RUNTIMES
+                )
+                satisfied_priorities = Insight.average_metric(
+                    insight_set, "PrioritySatisfaction"
+                )
+
+                metric_values = [
+                    average_runtimes,
+                    average_ginis,
+                    minimum_ginis,
+                    maximum_ginis,
+                    satisfied_priorities,
+                ]
+
+                # Data processing for graph
+                for i, metric in enumerate(metric_values):
+                    for name, data in metric.items():
+                        if name not in graph_dicts[i]:
+                            graph_dicts[i][name] = GraphData(
+                                x_data=[class_size],
+                                y_data=[data],
+                                name=name,
+                            )
+                        else:
+                            graph_dicts[i][name].x_data.append(class_size)
+                            graph_dicts[i][name].y_data.append(data)
+
+            line_graph(
+                LineGraphMetadata(
+                    x_label="Class size",
+                    y_label="Run time (seconds)",
+                    title="Diversify Gender With Min of Two Runtimes",
+                    data=list(graph_runtime_dict.values()),
+                )
+            )
+
+            line_graph(
+                LineGraphMetadata(
+                    x_label="Class size",
+                    y_label="Average Gini Index",
+                    title="Diversify Gender With Min of Two Average Gini Index",
+                    data=list(graph_avg_gini_dict.values()),
+                    y_lim=GraphAxisRange(
+                        *metrics["AverageGiniIndex"].theoretical_range
+                    ),
+                )
+            )
+
+            line_graph(
+                LineGraphMetadata(
+                    x_label="Class size",
+                    y_label="Minimum Gini Index",
+                    title="Diversify Gender With Min of Two Minimum Gini",
+                    data=list(graph_min_gini_dict.values()),
+                    y_lim=GraphAxisRange(*metrics["MinGiniIndex"].theoretical_range),
+                )
+            )
+
+            line_graph(
+                LineGraphMetadata(
+                    x_label="Class size",
+                    y_label="Maximum Gini Index",
+                    title="Diversify Gender With Min of Two Max Gini",
+                    data=list(graph_max_gini_dict.values()),
+                    y_lim=GraphAxisRange(*metrics["MaxGiniIndex"].theoretical_range),
+                )
+            )
+
+            line_graph(
+                LineGraphMetadata(
+                    x_label="Class size",
+                    y_label="Priorities Satisfied",
+                    title="Diversity Gender With Min of Two Satisfied Priorities",
+                    data=list(graph_priority_dict.values()),
+                    y_lim=GraphAxisRange(
+                        *metrics["PrioritySatisfaction"].theoretical_range
+                    ),
+                )
+            )
 
 
 if __name__ == "__main__":
-    typer.run(diversify_gender_min_2)
+    typer.run(DiversifyGenderMin2Run.start)
--- benchmarking/runs/include_social_friends.py
+++ benchmarking/runs/include_social_friends.py
@@ -28,111 +28,116 @@ from benchmarking.evaluations.metrics.utils.team_calculations import (
 from benchmarking.evaluations.scenarios.include_social_friends import (
     IncludeSocialFriends,
 )
+from benchmarking.runs.interfaces import Run
 from benchmarking.simulation.insight import Insight
 from benchmarking.simulation.simulation_set import SimulationSet, SimulationSetArtifact
 from benchmarking.simulation.simulation_settings import SimulationSettings
 
 
-def include_social_friends(num_trials: int = 10, generate_graphs: bool = True):
-    """
-    Goal: Run including social friends, measure average social satisfied team
-    (a team socially satisfied when all member is happy)
-    """
+class IncludeSocialFriendsRun(Run):
+    @staticmethod
+    def start(num_trials: int = 10, generate_graphs: bool = True):
+        """
+        Goal: Run including social friends, measure average social satisfied team
+        (a team socially satisfied when all member is happy)
+        """
 
-    # Defining our changing x-values (in the graph sense)
-    class_sizes = list(range(50, 501, 50))
+        # Defining our changing x-values (in the graph sense)
+        class_sizes = list(range(50, 501, 50))
 
-    # Graph variables
-    graph_runtime_dict = {}
-    graph_social_sat_dict = {}
-    graph_dicts = [graph_runtime_dict, graph_social_sat_dict]
+        # Graph variables
+        graph_runtime_dict = {}
+        graph_social_sat_dict = {}
+        graph_dicts = [graph_runtime_dict, graph_social_sat_dict]
 
-    metrics: Dict[str, TeamSetMetric] = {
-        "AverageSocialSatisfaction": AverageSocialSatisfaction(
-            metric_function=is_happy_team_allhp_friend
-        )
-    }
+        metrics: Dict[str, TeamSetMetric] = {
+            "AverageSocialSatisfaction": AverageSocialSatisfaction(
+                metric_function=is_happy_team_allhp_friend
+            )
+        }
 
-    artifacts: Dict[int, SimulationSetArtifact] = {}
+        artifacts: Dict[int, SimulationSetArtifact] = {}
 
-    for class_size in class_sizes:
-        print("CLASS SIZE /", class_size)
+        for class_size in class_sizes:
+            print("CLASS SIZE /", class_size)
 
-        number_of_teams = math.ceil(class_size / 5)
+            number_of_teams = math.ceil(class_size / 5)
 
-        # set up either mock or real data
-        student_provider_settings = MockStudentProviderSettings(
-            number_of_students=class_size,
-            number_of_friends=2,
-            number_of_enemies=2,
-            friend_distribution="cluster",
-        )
-
-        simulation_set_artifact = SimulationSet(
-            settings=SimulationSettings(
-                num_teams=number_of_teams,
-                scenario=IncludeSocialFriends(),
-                student_provider=MockStudentProvider(student_provider_settings),
-                cache_key=f"include_social_friends_{number_of_teams}",
-            ),
-            algorithm_set={
-                AlgorithmType.RANDOM: [RandomAlgorithmConfig()],
-                AlgorithmType.SOCIAL: [SocialAlgorithmConfig()],
-                AlgorithmType.WEIGHT: [WeightAlgorithmConfig()],
-                AlgorithmType.PRIORITY: [
-                    PriorityAlgorithmConfig(),
-                    PriorityAlgorithmConfig(
-                        name="local_max",
-                        MUTATIONS=[(mutate_local_max, 1), (mutate_random_swap, 2)],
-                    ),
-                ],
-            },
-        ).run(num_runs=num_trials)
-        artifacts[class_size] = simulation_set_artifact
-
-    if generate_graphs:
-        for class_size, artifact in artifacts.items():
-            insight_set: Dict[str, Dict[str, List[float]]] = Insight.get_output_set(
-                artifact=artifact, metrics=list(metrics.values())
+            # set up either mock or real data
+            student_provider_settings = MockStudentProviderSettings(
+                number_of_students=class_size,
+                number_of_friends=2,
+                number_of_enemies=2,
+                friend_distribution="cluster",
             )
 
-            average_runtimes = Insight.average_metric(insight_set, Insight.KEY_RUNTIMES)
-            average_social_satisfaction = Insight.average_metric(
-                insight_set, "AverageSocialSatisfaction"
-            )
-            metric_values = [average_runtimes, average_social_satisfaction]
+            simulation_set_artifact = SimulationSet(
+                settings=SimulationSettings(
+                    num_teams=number_of_teams,
+                    scenario=IncludeSocialFriends(),
+                    student_provider=MockStudentProvider(student_provider_settings),
+                    cache_key=f"include_social_friends_{number_of_teams}",
+                ),
+                algorithm_set={
+                    AlgorithmType.RANDOM: [RandomAlgorithmConfig()],
+                    AlgorithmType.SOCIAL: [SocialAlgorithmConfig()],
+                    AlgorithmType.WEIGHT: [WeightAlgorithmConfig()],
+                    AlgorithmType.PRIORITY: [
+                        PriorityAlgorithmConfig(),
+                        PriorityAlgorithmConfig(
+                            name="local_max",
+                            MUTATIONS=[(mutate_local_max, 1), (mutate_random_swap, 2)],
+                        ),
+                    ],
+                },
+            ).run(num_runs=num_trials)
+            artifacts[class_size] = simulation_set_artifact
 
-            # Data processing for graph
-            for i, metric in enumerate(metric_values):
-                for name, data in metric.items():
-                    if name not in graph_dicts[i]:
-                        graph_dicts[i][name] = GraphData(
-                            x_data=[class_size],
-                            y_data=[data],
-                            name=name,
-                        )
-                    else:
-                        graph_dicts[i][name].x_data.append(class_size)
-                        graph_dicts[i][name].y_data.append(data)
+        if generate_graphs:
+            for class_size, artifact in artifacts.items():
+                insight_set: Dict[str, Dict[str, List[float]]] = Insight.get_output_set(
+                    artifact=artifact, metrics=list(metrics.values())
+                )
 
-        line_graph(
-            LineGraphMetadata(
-                x_label="Class size",
-                y_label="Run time (seconds)",
-                title="Simulate including friends",
-                data=list(graph_runtime_dict.values()),
-            )
-        )
+                average_runtimes = Insight.average_metric(
+                    insight_set, Insight.KEY_RUNTIMES
+                )
+                average_social_satisfaction = Insight.average_metric(
+                    insight_set, "AverageSocialSatisfaction"
+                )
+                metric_values = [average_runtimes, average_social_satisfaction]
 
-        line_graph(
-            LineGraphMetadata(
-                x_label="Class size",
-                y_label="Happy Team - Each Member Has One Friend On Team",
-                title="Simulate including friends",
-                data=list(graph_social_sat_dict.values()),
+                # Data processing for graph
+                for i, metric in enumerate(metric_values):
+                    for name, data in metric.items():
+                        if name not in graph_dicts[i]:
+                            graph_dicts[i][name] = GraphData(
+                                x_data=[class_size],
+                                y_data=[data],
+                                name=name,
+                            )
+                        else:
+                            graph_dicts[i][name].x_data.append(class_size)
+                            graph_dicts[i][name].y_data.append(data)
+
+            line_graph(
+                LineGraphMetadata(
+                    x_label="Class size",
+                    y_label="Run time (seconds)",
+                    title="Simulate including friends",
+                    data=list(graph_runtime_dict.values()),
+                )
             )
-        )
+
+            line_graph(
+                LineGraphMetadata(
+                    x_label="Class size",
+                    y_label="Happy Team - Each Member Has One Friend On Team",
+                    title="Simulate including friends",
+                    data=list(graph_social_sat_dict.values()),
+                )
+            )
 
 
 if __name__ == "__main__":
-    typer.run(include_social_friends)
+    typer.run(IncludeSocialFriendsRun.start)
