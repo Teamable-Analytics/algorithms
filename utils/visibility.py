cbe54f0fe221b39d60519a92af0216cd849ccfa2
opeyem1a <opeyadeyemi@gmail.com>
2023-11-13 01:02:53 -0700

    lint

--- utils/visibility.py
+++ utils/visibility.py
@@ -8,8 +8,18 @@ def show_team_set(team_set: TeamSet, highlight_students_by: Callable):
     print(team_set.id)
     for team in team_set.teams:
         print("\t" + str(team.id))
-        print("\t\t" + str([f"{s.id}{'*' if highlight_students_by(s) else ''}" for s in team.students]))
+        print(
+            "\t\t"
+            + str(
+                [
+                    f"{s.id}{'*' if highlight_students_by(s) else ''}"
+                    for s in team.students
+                ]
+            )
+        )
 
 
-def highlight_with_attribute_value(student: Student, attribute_id: int, value: int) -> bool:
+def highlight_with_attribute_value(
+    student: Student, attribute_id: int, value: int
+) -> bool:
     return value in student.attributes.get(attribute_id, [])
