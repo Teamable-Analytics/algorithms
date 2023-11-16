ae7043de3fdc1e87156716dad4db90d5a342f3fa
Hedgemon4 <sethlinc4@gmail.com>
2023-11-16 10:33:20 -0800

    Changed file from team to class size

--- benchmarking/runs/priority_algorithm/diversify_gender_min_2/class_size.py
+++ benchmarking/runs/priority_algorithm/diversify_gender_min_2/class_size.py
@@ -49,7 +49,7 @@ class DiversifyGenderMin2ClassSize(Run):
         """
 
         # Defining our changing x-values (in the graph sense)
-        class_sizes = list(range(50, 201, 50))
+        class_sizes = list(range(50, 501, 50))
         ratio_of_female_students = 0.2
         max_time = 10
         max_iterate = 300
