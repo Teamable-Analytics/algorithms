1ca80b954f661dbf80de20ac7d7b95569df8a3c6
ketphan02 <tuankietvn@outlook.com>
2023-11-15 20:13:46 -0800

    EF1 implementation

--- benchmarking/evaluations/metrics/envy_free_up_to_one.py
+++ benchmarking/evaluations/metrics/envy_free_up_to_one.py
@@ -1,8 +1,11 @@
+import copy
+
+from api.models.team import Team
 from api.models.team_set import TeamSet
 from benchmarking.evaluations.interfaces import TeamSetMetric
 
 
-class EnvyFreeUpToOne(TeamSetMetric):
+class EnvyFreenessUpToOne(TeamSetMetric):
     """
     Calculate the envy-free up to one metric for the given attribute for each team and average the results
 
@@ -10,9 +13,52 @@ class EnvyFreeUpToOne(TeamSetMetric):
     Meaning if A is envying B, then A will no longer envy B if we remove one unit of the given attribute from  B
     """
 
-    def __init__(self, attribute: int, *args, **kwargs):
-        super().__init__("EF1", *args, **kwargs)
-        self.attribute = attribute
+    def __init__(self, *args, **kwargs):
+        super().__init__(name="EF1", *args, **kwargs)
 
-    def calculate(self, team_set: TeamSet) -> float:
-        pass
+    def calculate(self, team_set: TeamSet) -> int:
+        """
+        If envy-free up to one, return 1, else return 0
+        """
+        for team in team_set.teams:
+            for other_team in team_set.teams:
+                if team == other_team:
+                    continue
+
+                if not self._is_envy(team, other_team):
+                    continue
+
+                envy_up_to_one = False
+                for student_idx, student in enumerate(team.students):
+                    team_copy = copy.deepcopy(team)
+                    team_copy.students.pop(student_idx)
+                    if not self._is_envy(team, other_team):
+                        envy_up_to_one = True
+                        break
+                if not envy_up_to_one:
+                    return 0
+        return 1
+
+    def _is_envy(self, team: Team, other_team: Team) -> bool:
+        """
+        Check if team envies other_team
+        """
+        team_utility = self._calculate_team_utility(team)
+        other_team_utility = self._calculate_team_utility(other_team)
+        if team_utility < other_team_utility:
+            return True
+        return False
+
+    def _calculate_team_utility(self, team: Team) -> int:
+        """
+        Calculate additive utility of all students in the team
+
+        Utility of a student is calculated as the sum of the student's satisfied attributes to the requirements
+        """
+        team_utility = 0
+        for student in team.students:
+            team_utility += sum([
+                student.meets_requirement(requirement)
+                for requirement in team.requirements
+            ])
+        return team_utility
