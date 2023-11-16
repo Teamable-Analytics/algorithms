be4c9adf41b312f8b93e9d8bd0f7f476fdfafba0
ketphan02 <tuankietvn@outlook.com>
2023-11-15 22:49:17 -0800

    Passing lambda to the class

--- benchmarking/evaluations/metrics/envy_free_up_to_one_item.py
+++ benchmarking/evaluations/metrics/envy_free_up_to_one_item.py
@@ -1,14 +1,14 @@
 import copy
+from typing import Callable, Dict
 
 from api.models.team import Team
 from api.models.team_set import TeamSet
 from benchmarking.evaluations.interfaces import TeamSetMetric
 
 
-class EnvyFreenessUpToOneWithRequirementUtility(TeamSetMetric):
+class EnvyFreenessUpToOneItem(TeamSetMetric):
     """
-    Calculate the "envy-freeness up to one item" metric for the given attribute using requirement utility (team
-    satisfaction) for each team and average the results
+    Calculate the "envy-freeness up to one item" metric for the given attribute for each team and average the results
 
     ----
 
@@ -18,8 +18,9 @@ class EnvyFreenessUpToOneWithRequirementUtility(TeamSetMetric):
     - Envy-free up to one item (ef1): Team B ef1 Team A if Team B envies Team A and if Team A removes one student, Team B no longer envies Team A
     """
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, calculate_utilities: Callable[[TeamSet], Dict[int, Dict[int]]], *args, **kwargs):
         super().__init__(theoretical_range=(0, 1), *args, **kwargs)
+        self.calculate_utilities = calculate_utilities
 
     def calculate(self, team_set: TeamSet) -> float:
         """
@@ -30,46 +31,38 @@ class EnvyFreenessUpToOneWithRequirementUtility(TeamSetMetric):
         Note: The return of this function should be `bool`, but we return
         `float` to calculate the average between multiple runs
         """
+        utilities = self.calculate_utilities(team_set)
+
         for team in team_set.teams:
             for other_team in team_set.teams:
                 if team == other_team:
                     continue
 
-                if not self._is_envy(other_team, team):
+                if not self._is_envy(other_team, team, utilities):
                     continue
 
                 envy_up_to_one = False
                 for student_idx, student in enumerate(team.students):
                     team_copy = copy.deepcopy(team)
                     team_copy.students.pop(student_idx)
-                    if not self._is_envy(team, other_team):
+                    if not self._is_envy(team, other_team, utilities):
                         envy_up_to_one = True
                         break
                 if not envy_up_to_one:
                     return 0
         return 1
 
-    def _is_envy(self, team: Team, other_team: Team) -> bool:
+    def _is_envy(self, team: Team, other_team: Team, utilities: Dict[int, Dict[int]]) -> bool:
         """
         Check if team envies other_team
         """
-        team_utility = self._calculate_team_requirement_satisfaction(team)
-        other_team_utility = self._calculate_team_requirement_satisfaction(other_team)
+        team_utility = self._calculate_team_utility(team, utilities)
+        other_team_utility = self._calculate_team_utility(other_team, utilities)
 
         return team_utility < other_team_utility
 
-    def _calculate_team_requirement_satisfaction(self, team: Team) -> int:
-        """
-        Calculate team requirement satisfaction (additive requirement utility) of all students in the team
-
-        Team requirement satisfaction is calculated as the sum of the satisfied requirements of all students in the team
-        """
-        team_requirement_satisfaction = 0
-        for student in team.students:
-            team_requirement_satisfaction += sum(
-                [
-                    student.meets_requirement(requirement)
-                    for requirement in team.requirements
-                ]
-            )
-        return team_requirement_satisfaction
+    def _calculate_team_utility(self, team: Team, utilities: Dict[int, Dict[int]]) -> int:
+        return sum([
+            utilities[team.project_id][student.id]
+            for student in team.students
+        ])
