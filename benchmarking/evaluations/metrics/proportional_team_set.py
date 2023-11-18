862f271d5cca6201fa86dc89b1a213faac3bdb92
ketphan02 <tuankietvn@outlook.com>
2023-11-17 17:54:33 -0800

    Implement PROP

---
+++ benchmarking/evaluations/metrics/proportional_team_set.py
@@ -0,0 +1,41 @@
+from typing import Callable, List
+
+from api.models.student import Student
+from api.models.team import TeamShell
+from api.models.team_set import TeamSet
+from benchmarking.evaluations.interfaces import TeamSetMetric
+
+
+class ProportionalTeamSet(TeamSetMetric):
+    """
+    Calculate the proportional (PROP) metric for team set average the results
+
+    ----
+
+    Definition:
+
+    - PROP: A team set is proportional if the utility of each team is at least the fair share of
+    the class utility with the team's requirements.
+    """
+
+    def __init__(
+            self,
+            calculate_utilities: Callable[[List[Student], TeamShell], float],
+            *args,
+            **kwargs
+    ):
+        super().__init__(theoretical_range=(0, 1), *args, **kwargs)
+        self.calculate_utilities = calculate_utilities
+
+    def calculate(self, team_set: TeamSet) -> float:
+        all_students = [student for team in team_set.teams for student in team.students]
+
+        for team in team_set.teams:
+            class_utility = self.calculate_utilities(all_students, team.to_shell())
+            team.utility = self.calculate_utilities(team.students, team.to_shell())
+
+            expected_team_utility = class_utility / float(team_set.num_teams)
+            if team.utility < expected_team_utility:
+                return 0.0
+
+        return 1.0
---
+++ tests/test_benchmarking/test_evaluations/test_metrics/test_proportional_team_set.py
@@ -0,0 +1,151 @@
+import unittest
+
+from api.models.enums import RequirementOperator
+from api.models.project import ProjectRequirement
+from api.models.student import Student
+from api.models.team import Team
+from api.models.team_set import TeamSet
+from benchmarking.evaluations.metrics.proportional_team_set import ProportionalTeamSet
+
+
+class TestProportionalTeamSet(unittest.TestCase):
+    def setUp(self):
+
+        # This teamset is proportional because each team has 2 students that meet the requirements,
+        # so the utility of each team is the same, and proportional to the class utility
+        self.prop_teamset = TeamSet(
+            _id=0,
+            teams=[
+                Team(
+                    _id=0,
+                    requirements=[
+                        ProjectRequirement(
+                            attribute=0,
+                            value=1,
+                            operator=RequirementOperator.EXACTLY,
+                        ),
+                    ],
+                    students=[
+                        Student(
+                            _id=0,
+                            attributes={
+                                0: [1],
+                            },
+                        ),
+                        Student(
+                            _id=1,
+                            attributes={
+                                0: [1],
+                            },
+                        )
+                    ],
+                ),
+                Team(
+                    _id=1,
+                    requirements=[
+                        ProjectRequirement(
+                            attribute=0,
+                            value=1,
+                            operator=RequirementOperator.EXACTLY,
+                        ),
+                    ],
+                    students=[
+                        Student(
+                            _id=2,
+                            attributes={
+                                0: [1],
+                            },
+                        ),
+                        Student(
+                            _id=3,
+                            attributes={
+                                0: [1],
+                            },
+                        )
+                    ],
+                ),
+            ],
+        )
+
+        # This teamset is not proportional because Team 0 has 2 students that meet the requirements while Team 1 has
+        # only 1 student that meets the requirements, therefore the utility of Team 1 is not proportional to the class
+        # utility
+        self.non_prop_teamset = TeamSet(
+            _id=0,
+            teams=[
+                Team(
+                    _id=0,
+                    requirements=[
+                        ProjectRequirement(
+                            attribute=0,
+                            value=1,
+                            operator=RequirementOperator.EXACTLY,
+                        ),
+                    ],
+                    students=[
+                        Student(
+                            _id=0,
+                            attributes={
+                                0: [1],
+                            },
+                        ),
+                        Student(
+                            _id=1,
+                            attributes={
+                                0: [1],
+                            },
+                        )
+                    ],
+                ),
+                Team(
+                    _id=1,
+                    requirements=[
+                        ProjectRequirement(
+                            attribute=0,
+                            value=1,
+                            operator=RequirementOperator.EXACTLY,
+                        ),
+                    ],
+                    students=[
+                        Student(
+                            _id=2,
+                            attributes={
+                                0: [1],
+                            },
+                        ),
+                        Student(
+                            _id=3,
+                            attributes={
+                                0: [2],
+                            },
+                        )
+                    ],
+                ),
+            ],
+        )
+
+        self.additive_utility_function = lambda students, team: sum(
+            [
+                sum(
+                    [
+                        student.meets_requirement(requirement)
+                        for requirement in team.requirements
+                    ]
+                )
+                for student in students
+            ]
+        )
+
+    def test_calculate__should_return_1_when_there_is_only_one_team(self):
+        # Because the team is always proportional to itself
+        single_team_teamset = TeamSet(_id=0, teams=[Team(_id=0)])
+        metric = ProportionalTeamSet(self.additive_utility_function)
+        self.assertEqual(metric.calculate(single_team_teamset), 1.0)
+
+    def test_calculate__should_return_1_if_all_teams_are_proportional(self):
+        metric = ProportionalTeamSet(self.additive_utility_function)
+        self.assertEqual(metric.calculate(self.prop_teamset), 1.0)
+
+    def test_calculate__should_return_0_if_any_team_is_not_proportional(self):
+        metric = ProportionalTeamSet(self.additive_utility_function)
+        self.assertEqual(metric.calculate(self.non_prop_teamset), 0.0)
