d5452e6d69e6d2829f2e5f3c58b116f4b51ee08b
ketphan02 <tuankietvn@outlook.com>
2023-11-17 19:29:03 -0800

    Adding PROP1

---
+++ benchmarking/evaluations/metrics/proportionality_up_to_one_item.py
@@ -0,0 +1,101 @@
+from typing import Callable, List
+
+from api.models.student import Student
+from api.models.team import TeamShell, Team
+from api.models.team_set import TeamSet
+from benchmarking.evaluations.interfaces import TeamSetMetric
+
+
+class ProportionalityUpToOneItem(TeamSetMetric):
+    def __init__(
+        self,
+        calculate_utilities: Callable[[List[Student], TeamShell], float],
+        *args,
+        **kwargs
+    ):
+        super().__init__(theoretical_range=(0, 1), *args, **kwargs)
+        self.calculate_utilities = calculate_utilities
+
+    def calculate(self, team_set: TeamSet) -> float:
+        all_students = [student for team in team_set.teams for student in team.students]
+
+        return (
+            self._is_prop(team_set, all_students)
+            or self._is_prop_with_one_extra_student(team_set, all_students)
+            or self._is_prop_with_one_less_student(team_set, all_students)
+        )
+
+    def _is_prop(self, team_set: TeamSet, all_students: List[Student]) -> bool:
+        return all(
+            [
+                self._is_team_prop(team, all_students, team_set.num_teams)
+                for team in team_set.teams
+            ]
+        )
+
+    def _is_prop_with_one_extra_student(
+        self, team_set: TeamSet, all_students: List[Student]
+    ) -> bool:
+        for team in team_set.teams:
+            students_in_team = set([student.id for student in team.students])
+
+            is_new_team_prop = all(
+                [
+                    self._is_team_prop(
+                        Team(
+                            _id=team.id,
+                            students=team.students + [student],
+                        ),
+                        all_students,
+                        team_set.num_teams,
+                    )
+                    for team in team_set.teams
+                ]
+                for student in all_students
+                if student.id not in students_in_team
+            )
+
+            if not is_new_team_prop:
+                return False
+
+        return True
+
+    def _is_prop_with_one_less_student(
+        self, team_set: TeamSet, all_students: List[Student]
+    ) -> bool:
+        for team in team_set.teams:
+            is_new_team_prop = all(
+                [
+                    [
+                        self._is_team_prop(
+                            Team(
+                                _id=team.id,
+                                students=[
+                                    student_in_team
+                                    for student_in_team in team.students
+                                    if student_in_team.id != student.id
+                                ],
+                            ),
+                            all_students,
+                            team_set.num_teams,
+                        )
+                        for team in team_set.teams
+                    ]
+                    for student in team.students
+                ]
+            )
+
+            if not is_new_team_prop:
+                return False
+
+        return True
+
+    def _is_team_prop(
+        self, team: Team, all_students: List[Student], num_teams: int
+    ) -> bool:
+        class_utility = self.calculate_utilities(all_students, team.to_shell())
+        team.utility = self.calculate_utilities(team.students, team.to_shell())
+
+        expected_team_utility = class_utility / float(num_teams)
+        if team.utility < expected_team_utility:
+            return False
