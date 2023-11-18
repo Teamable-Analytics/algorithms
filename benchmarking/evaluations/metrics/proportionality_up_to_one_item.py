beb56447e008f004180d475fcd0df7562fcb9958
ketphan02 <tuankietvn@outlook.com>
2023-11-17 19:32:57 -0800

    Better name

--- benchmarking/evaluations/metrics/proportionality_up_to_one_item.py
+++ benchmarking/evaluations/metrics/proportionality_up_to_one_item.py
@@ -20,28 +20,28 @@ class ProportionalityUpToOneItem(TeamSetMetric):
         all_students = [student for team in team_set.teams for student in team.students]
 
         return (
-            self._is_prop(team_set, all_students)
-            or self._is_prop_with_one_extra_student(team_set, all_students)
-            or self._is_prop_with_one_less_student(team_set, all_students)
+            self._is_proportional(team_set, all_students)
+            or self._is_proportional_with_one_extra_student(team_set, all_students)
+            or self._is_proportional_with_one_less_student(team_set, all_students)
         )
 
-    def _is_prop(self, team_set: TeamSet, all_students: List[Student]) -> bool:
+    def _is_proportional(self, team_set: TeamSet, all_students: List[Student]) -> bool:
         return all(
             [
-                self._is_team_prop(team, all_students, team_set.num_teams)
+                self._is_team_proportional(team, all_students, team_set.num_teams)
                 for team in team_set.teams
             ]
         )
 
-    def _is_prop_with_one_extra_student(
+    def _is_proportional_with_one_extra_student(
         self, team_set: TeamSet, all_students: List[Student]
     ) -> bool:
         for team in team_set.teams:
             students_in_team = set([student.id for student in team.students])
 
-            is_new_team_prop = all(
+            is_new_team_proportional = all(
                 [
-                    self._is_team_prop(
+                    self._is_team_proportional(
                         Team(
                             _id=team.id,
                             students=team.students + [student],
@@ -55,19 +55,19 @@ class ProportionalityUpToOneItem(TeamSetMetric):
                 if student.id not in students_in_team
             )
 
-            if not is_new_team_prop:
+            if not is_new_team_proportional:
                 return False
 
         return True
 
-    def _is_prop_with_one_less_student(
+    def _is_proportional_with_one_less_student(
         self, team_set: TeamSet, all_students: List[Student]
     ) -> bool:
         for team in team_set.teams:
-            is_new_team_prop = all(
+            is_new_team_proportional = all(
                 [
                     [
-                        self._is_team_prop(
+                        self._is_team_proportional(
                             Team(
                                 _id=team.id,
                                 students=[
@@ -85,12 +85,12 @@ class ProportionalityUpToOneItem(TeamSetMetric):
                 ]
             )
 
-            if not is_new_team_prop:
+            if not is_new_team_proportional:
                 return False
 
         return True
 
-    def _is_team_prop(
+    def _is_team_proportional(
         self, team: Team, all_students: List[Student], num_teams: int
     ) -> bool:
         class_utility = self.calculate_utilities(all_students, team.to_shell())
