79d60d401460147c2d56e307ee443dd77cd65ba6
Hedgemon4 <sethlinc4@gmail.com>
2023-11-26 21:33:28 -0800

    Worked on making the runs more generalizable

--- benchmarking/runs/priority_algorithm/diversify_gender_min_2/interfaces.py
+++ benchmarking/runs/priority_algorithm/diversify_gender_min_2/interfaces.py
@@ -1,10 +1,28 @@
 from functools import cached_property
 from typing import Dict, List
 
-from api.models.enums import ScenarioAttribute, Gender
+from api.ai.interfaces.algorithm_config import (
+    RandomAlgorithmConfig,
+    WeightAlgorithmConfig,
+    PriorityAlgorithmConfig,
+)
+from api.ai.priority_algorithm.mutations import (
+    mutate_local_max,
+    mutate_random_swap,
+    mutate_robinhood_holistic,
+    mutate_robinhood,
+    mutate_local_max_double_random,
+    mutate_local_max_random,
+)
+from api.models.enums import ScenarioAttribute, Gender, AlgorithmType
 from benchmarking.data.simulated_data.mock_student_provider import (
     MockStudentProviderSettings,
+    MockStudentProvider,
 )
+from benchmarking.evaluations import metrics
+from benchmarking.evaluations.graphing.graph_metadata import GraphData, GraphAxisRange
+from benchmarking.evaluations.graphing.line_graph import line_graph
+from benchmarking.evaluations.graphing.line_graph_metadata import LineGraphMetadata
 from benchmarking.evaluations.interfaces import Scenario, TeamSetMetric
 from benchmarking.evaluations.metrics.average_gini_index import AverageGiniIndex
 from benchmarking.evaluations.metrics.maximum_gini_index import MaximumGiniIndex
@@ -15,7 +33,8 @@ from benchmarking.evaluations.scenarios.diversify_gender_min_2_female import (
 )
 from benchmarking.runs.interfaces import Run
 from benchmarking.simulation.goal_to_priority import goals_to_priorities
-from benchmarking.simulation.simulation_set import SimulationSet
+from benchmarking.simulation.insight import Insight
+from benchmarking.simulation.simulation_set import SimulationSet, SimulationSetArtifact
 from benchmarking.simulation.simulation_settings import SimulationSettings
 
 
@@ -53,10 +72,11 @@ class PriorityAlgorithmParameters(Run):
             ),
         }
 
-    @property
-    def mock_student_provider_settings(self):
+    def mock_student_provider_settings(self, **kwargs):
         return MockStudentProviderSettings(
-            number_of_students=self.NUMBER_OF_STUDENTS,
+            number_of_students=kwargs["number_students"]
+            if "number_students" in kwargs
+            else self.NUMBER_OF_STUDENTS,
             attribute_ranges={
                 ScenarioAttribute.GENDER.value: [
                     (Gender.MALE, 1 - self.RATIO_OF_FEMALE_STUDENT),
@@ -65,18 +85,24 @@ class PriorityAlgorithmParameters(Run):
             },
         )
 
-    @property
-    def simulation_settings(self, **kwargs):
-        return
-
-    @property
-    def simulation_set(self, **kwargs):
+    def simulation_set(self, cache_key: str, **kwargs):
+        max_iterate = (
+            kwargs["max_iterate"] if "max_iterate" in kwargs else self.MAX_ITERATE
+        )
+        max_spread = kwargs["max_spread"] if "max_spread" in kwargs else self.MAX_SPREAD
+        max_time = kwargs["max_time"] if "max_time" in kwargs else self.MAX_TIME
+        max_keep = kwargs["max_keep"] if "max_keep" in kwargs else self.MAX_KEEP
+        num_teams = (
+            kwargs["number_teams"] if "number_teams" in kwargs else self.NUMBER_OF_TEAMS
+        )
         return SimulationSet(
             settings=SimulationSettings(
-                num_teams=kwargs["number_of_teams"],
+                num_teams=num_teams,
                 scenario=self.scenario,
-                student_provider=self.mock_student_provider_settings(**kwargs),
-                cache_key=f"priority_algorithm/diversify_gender_min_2/max_spread/{spread}",
+                student_provider=MockStudentProvider(
+                    self.mock_student_provider_settings(**kwargs)
+                ),
+                cache_key=cache_key,
             ),
             algorithm_set={
                 AlgorithmType.RANDOM: [RandomAlgorithmConfig()],
@@ -86,64 +112,64 @@ class PriorityAlgorithmParameters(Run):
                         MAX_ITERATE=max_iterate,
                         MAX_TIME=max_time,
                         MAX_KEEP=max_keep,
-                        MAX_SPREAD=spread,
+                        MAX_SPREAD=max_spread,
                     ),
                     PriorityAlgorithmConfig(
                         name="local_max",
                         MUTATIONS=[
                             (mutate_local_max, 1),
-                            (mutate_random_swap, spread - 1),
+                            (mutate_random_swap, max_spread - 1),
                         ],
                         MAX_TIME=max_time,
                         MAX_ITERATE=max_iterate,
                         MAX_KEEP=max_keep,
-                        MAX_SPREAD=spread,
+                        MAX_SPREAD=max_spread,
                     ),
                     PriorityAlgorithmConfig(
                         name="local_max_random",
                         MUTATIONS=[
                             (mutate_local_max_random, 1),
-                            (mutate_random_swap, spread - 1),
+                            (mutate_random_swap, max_spread - 1),
                         ],
                         MAX_TIME=max_time,
                         MAX_ITERATE=max_iterate,
                         MAX_KEEP=max_keep,
-                        MAX_SPREAD=spread,
+                        MAX_SPREAD=max_spread,
                     ),
                     PriorityAlgorithmConfig(
                         name="local_max_double_random",
                         MUTATIONS=[
                             (mutate_local_max_double_random, 1),
-                            (mutate_random_swap, spread - 1),
+                            (mutate_random_swap, max_spread - 1),
                         ],
                         MAX_TIME=max_time,
                         MAX_ITERATE=max_iterate,
                         MAX_KEEP=max_keep,
-                        MAX_SPREAD=spread,
+                        MAX_SPREAD=max_spread,
                     ),
                     PriorityAlgorithmConfig(
                         name="local_max_pure_double_random",
-                        MUTATIONS=[(mutate_local_max_double_random, spread)],
+                        MUTATIONS=[(mutate_local_max_double_random, max_spread)],
                         MAX_TIME=max_time,
                         MAX_ITERATE=max_iterate,
                         MAX_KEEP=max_keep,
-                        MAX_SPREAD=spread,
+                        MAX_SPREAD=max_spread,
                     ),
                     PriorityAlgorithmConfig(
                         name="robinhood",
-                        MUTATIONS=[(mutate_robinhood, spread)],
+                        MUTATIONS=[(mutate_robinhood, max_spread)],
                         MAX_TIME=max_time,
                         MAX_ITERATE=max_iterate,
                         MAX_KEEP=max_keep,
-                        MAX_SPREAD=spread,
+                        MAX_SPREAD=max_spread,
                     ),
                     PriorityAlgorithmConfig(
                         name="robinhood_holistic",
-                        MUTATIONS=[(mutate_robinhood_holistic, spread)],
+                        MUTATIONS=[(mutate_robinhood_holistic, max_spread)],
                         MAX_TIME=max_time,
                         MAX_ITERATE=max_iterate,
                         MAX_KEEP=max_keep,
-                        MAX_SPREAD=spread,
+                        MAX_SPREAD=max_spread,
                     ),
                 ],
             },
@@ -152,3 +178,74 @@ class PriorityAlgorithmParameters(Run):
     @cached_property
     def scenario(self) -> Scenario:
         return DiversifyGenderMin2Female(value_of_female=Gender.FEMALE.value)
+
+    def generate_graphs(self, artifacts: Dict[int, SimulationSetArtifact]):
+        graph_runtime_dict = {}
+        graph_avg_gini_dict = {}
+        graph_priority_dict = {}
+        graph_dicts = [
+            graph_runtime_dict,
+            graph_avg_gini_dict,
+            graph_priority_dict,
+        ]
+
+        for class_size, artifact in artifacts.items():
+            insight_set: Dict[str, Dict[str, List[float]]] = Insight.get_output_set(
+                artifact=artifact, metrics=list(self.metrics.values())
+            )
+
+            average_ginis = Insight.average_metric(insight_set, "AverageGiniIndex")
+            average_runtimes = Insight.average_metric(insight_set, Insight.KEY_RUNTIMES)
+            satisfied_priorities = Insight.average_metric(
+                insight_set, "PrioritySatisfaction"
+            )
+
+            metric_values = [
+                average_runtimes,
+                average_ginis,
+                satisfied_priorities,
+            ]
+
+            # Data processing for graph
+            for i, metric in enumerate(metric_values):
+                for name, data in metric.items():
+                    if name not in graph_dicts[i]:
+                        graph_dicts[i][name] = GraphData(
+                            x_data=[class_size],
+                            y_data=[data],
+                            name=name,
+                        )
+                    else:
+                        graph_dicts[i][name].x_data.append(class_size)
+                        graph_dicts[i][name].y_data.append(data)
+
+        line_graph(
+            LineGraphMetadata(
+                x_label="Class size",
+                y_label="Run time (seconds)",
+                title="Diversify Gender With Min of Two Runtimes",
+                data=list(graph_runtime_dict.values()),
+            )
+        )
+
+        line_graph(
+            LineGraphMetadata(
+                x_label="Class size",
+                y_label="Average Gini Index",
+                title="Diversify Gender With Min of Two Average Gini Index",
+                data=list(graph_avg_gini_dict.values()),
+                y_lim=GraphAxisRange(*metrics["AverageGiniIndex"].theoretical_range),
+            )
+        )
+
+        line_graph(
+            LineGraphMetadata(
+                x_label="Class size",
+                y_label="Priorities Satisfied",
+                title="Diversity Gender With Min of Two Satisfied Priorities",
+                data=list(graph_priority_dict.values()),
+                y_lim=GraphAxisRange(
+                    *metrics["PrioritySatisfaction"].theoretical_range
+                ),
+            )
+        )
