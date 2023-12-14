9d2343f5f9e8a681ea6948cc1d3d83f047fe1775
ketphan02 <tuankietvn@outlook.com>
2023-12-14 02:12:05 -0800

    No exposed secret keys

--- api/api/settings/prod.py
+++ api/api/settings/prod.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-b-q$3f0wrv39^f^mj(vo6$%*y!y47ygzp(q)xkkcvj^z(4#-w5"
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
