1990595565a5c4131b84c47b17647d4d4f0a63dd
ketphan02 <tuankietvn@outlook.com>
2023-12-14 01:49:07 -0800

    Allow cors

--- api/api/settings/prod.py
+++ api/api/settings/prod.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "api",
     "rest_framework",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
@@ -51,6 +52,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = "api.api.urls"
@@ -88,6 +90,11 @@ AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
+]
+
+# CORS
+CORS_ALLOWED_ORIGINS = [
+    "*"
 ]
 
 
