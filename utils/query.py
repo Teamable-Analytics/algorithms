940bd288a9fa80cc3fdb2a63a7975ea8433526c8
Seth Akins <sethlinc4@gmail.com>
2023-12-23 11:43:38 -0700

    Added some documentation about the function

--- utils/query.py
+++ utils/query.py
@@ -1,10 +1,20 @@
 import json
+from typing import List
+
 import jmespath
 
 
 def num_of_each_attribute_per_team(
-    file_path: str, attribute: int, attribute_value: int
-):
+    file_path: str, attribute: int, attribute_value: any
+) -> List[List]:
+    """
+    Used to count the number of students on each team that has a given attribute.
+    The function returns a list of lists where each inner list is the count of students on each team in the team set
+    that have the attribute specified in the function parameters.
+    num_of_each_attribute_per_team('path', 1, 2) will count how many students have the value 2 in their attribute array
+    with key 1.
+    Note that this function will only work if the attribute array key is numeric.
+    """
     with open(file_path) as f:
         json_data = json.load(f)
     return [
