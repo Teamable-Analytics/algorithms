cdc30abeba2e60399d868aa0abea5ba72612b7fc
ketphan02 <tuankietvn@outlook.com>
2024-01-02 20:25:28 -0800

    New set

--- benchmarking/runs/priority_algorithm/project_scenario/project_scenario.py
+++ benchmarking/runs/priority_algorithm/project_scenario/project_scenario.py
@@ -70,7 +70,7 @@ class StudentSatisfyProjectRequirements(Run):
         # Ranges
         max_keep_range = [5, 100, 250, 500, 750, 1000]
         max_spread_range = [1, 25, 50, 75, 100]
-        max_iterations_range = [10]
+        max_iterations_range = [1500]
 
         completed_configs_dict = {
             "weight": [],
