828a76132071508f7a92cf42b6712eeafb72f10d
ketphan02 <tuankietvn@outlook.com>
2024-01-09 23:27:14 -0800

    Add middleware for authentication

--- api/api/settings/__init__.py
+++ api/api/settings/__init__.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "api.api.middlewares.api_key_authentication.APIKeyAuthenticationMiddleware",
 ]
 
 ROOT_URLCONF = "api.api.urls"
