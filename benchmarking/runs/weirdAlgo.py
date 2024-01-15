b5cf7f8de54735cc33a73aff002b5050b328585d
ketphan02 <tuankietvn@outlook.com>
2024-01-15 06:12:38 -0800

    Multithreads

--- benchmarking/runs/weirdAlgo.py
+++ benchmarking/runs/weirdAlgo.py
@@ -69,7 +69,7 @@ def load_data():
     their_scores = []
     our_scores = []
 
-    NUM_DATAPOINT = 1
+    NUM_DATAPOINT = 1000
 
     for idx in range(NUM_DATAPOINT):
         df = pd.read_csv(f'their_data/out-private-{idx+1}.csv')
@@ -149,6 +149,80 @@ def load_data():
     plt.show()
 
 
+def run_and_save(idx):
+    df = pd.read_csv(f'their_data/out-private-{idx + 1}.csv')
+    teams = {}
+    for _, row in df.iterrows():
+        new_student = Student(
+            _id=row['sid'],
+            name=row['first_name'] + ' ' + row['last_name'],
+            attributes={
+                ScenarioAttribute.YEAR_LEVEL.value: [int(row['year'])],
+                ScenarioAttribute.RACE.value: [fromAlRaceToRace(int(row['race'])).value],
+                ScenarioAttribute.GENDER.value: [fromAlGenderToGender(int(row['gender'])).value],
+                ScenarioAttribute.TIMESLOT_AVAILABILITY.value: list(
+                    map(int, row['disc_times_options'].strip("[']").split(','))),
+            }
+        )
+        if row['group_num'] not in teams:
+            teams[row['group_num']] = Team(_id=row['group_num'])
+        teams[row['group_num']].add_student(new_student)
+
+    weird_algo_teamset = TeamSet(teams=list(teams.values()))
+
+    all_students = [student for team in weird_algo_teamset.teams for student in team.students]
+    random.shuffle(all_students)
+
+    scenario = ConcentrateTimeAvailabilityDiversifyGenderMin2Female(Gender.FEMALE.value)
+    priority_algorithm = PriorityAlgorithm(
+        algorithm_options=PriorityAlgorithmOptions(
+            priorities=goals_to_priorities(scenario.goals),
+            attributes_to_concentrate=[ScenarioAttribute.TIMESLOT_AVAILABILITY.value],
+            attributes_to_diversify=[ScenarioAttribute.GENDER.value],
+            max_project_preferences=0,
+        ),
+        algorithm_config=PriorityAlgorithmConfig(
+            MAX_KEEP=100,
+            MAX_SPREAD=100,
+            MAX_ITERATE=1500,
+            MAX_TIME=10000,
+        ),
+        team_generation_options=TeamGenerationOptions(
+            max_team_size=6,
+            min_team_size=3,
+            total_teams=weird_algo_teamset.num_teams,
+            initial_teams=[],
+        )
+    )
+
+    our_teamset = priority_algorithm.generate(all_students)
+
+    ok_dict = {}
+    for team in our_teamset.teams:
+        for student in team.students:
+            ok_dict['sid'] = student.id
+            ok_dict['group_num'] = team.id
+            ok_dict['first_name'] = student.name.split(' ')[0]
+            ok_dict['last_name'] = student.name.split(' ')[1]
+            ok_dict['year'] = fromYearLevelToAlYearLevel(
+                student.attributes[ScenarioAttribute.YEAR_LEVEL.value][0]).value
+            ok_dict['gender'] = fromGenderToAlGender(
+                Gender(student.attributes[ScenarioAttribute.GENDER.value][0])).value
+            ok_dict['race'] = fromRaceToAlRace(Race(student.attributes[ScenarioAttribute.RACE.value][0])).value
+            ok_dict['disc_times_options'] = fromNumbersToTimeSlots(
+                student.attributes[ScenarioAttribute.TIMESLOT_AVAILABILITY.value])
+
+    df = pd.DataFrame.from_dict(ok_dict)
+    df.to_csv(f'our_data/our{idx + 1}.csv')
+
+
+def run_and_save_multithreads():
+    import concurrent.futures
+    with concurrent.futures.ThreadPoolExecutor(max_workers=30) as executor:
+        executor.map(run_and_save, range(1000))
+
+
 if __name__ == "__main__":
-    load_data()
+    # load_data()
     # generate_data()
+    run_and_save_multithreads()
