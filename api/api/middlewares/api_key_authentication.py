c08934bd4172ecc685ab10bf41c3b9efe837b840
K (Kiet) Phan <35695324+ketphan02@users.noreply.github.com>
2024-01-16 09:58:09 -0800

    Add API key validation (#315)

---
+++ api/api/middlewares/api_key_authentication.py
@@ -0,0 +1,20 @@
+from django.http import HttpResponse
+
+from utils.api_auth import is_api_key_valid
+
+
+class APIKeyAuthenticationMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        response = self.get_response(request)
+
+        request_headers = dict(request.headers)
+        api_key = request_headers.get("X-Api-Key")
+        if not api_key:
+            return HttpResponse("No API key provided", status=400)
+        if not is_api_key_valid(api_key):
+            return HttpResponse("Invalid API key provided", status=401)
+
+        return response
--- api/api/settings/prod.py
+++ api/api/settings/prod.py
@@ -54,6 +54,7 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "corsheaders.middleware.CorsMiddleware",
+    "api.api.middlewares.api_key_authentication.APIKeyAuthenticationMiddleware",
 ]
 
 ROOT_URLCONF = "api.api.urls"
