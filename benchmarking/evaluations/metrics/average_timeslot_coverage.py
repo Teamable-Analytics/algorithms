d107b0dcfa31807ee6acb1392ac7c1e7bb551639
ketphan02 <tuankietvn@outlook.com>
2024-01-19 21:17:29 -0800

    Timeslot coverage metric

---
+++ benchmarking/evaluations/metrics/average_timeslot_coverage.py
@@ -0,0 +1,40 @@
+from typing import List
+
+from api.models.enums import ScenarioAttribute
+from api.models.team_set import TeamSet
+from benchmarking.evaluations.interfaces import TeamSetMetric
+
+
+class AverageTimeslotCoverage(TeamSetMetric):
+    """
+    Calculate the average mutual timeslots between students in a team and average over all teams.
+    """
+
+    def __init__(self, available_timeslots: List[int],
+                 timeslot_attribute_value: int = ScenarioAttribute.TIMESLOT_AVAILABILITY.value,
+                 *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.available_timeslots = available_timeslots
+        self.timeslot_attribute_value = timeslot_attribute_value
+
+        if len(available_timeslots) == 0:
+            raise ValueError("There must be at least one available timeslot")
+
+    def calculate(self, team_set: TeamSet) -> float:
+        if len(team_set.teams) == 0:
+            raise ValueError("Team set must have at least one team")
+
+        for student in [student for team in team_set.teams for student in team.students]:
+            if student.attributes.get(self.timeslot_attribute_value) is None:
+                raise ValueError(f"Student {student.id} does not have the timeslot attribute")
+            if any([attr not in self.available_timeslots for attr in student.attributes.get(self.timeslot_attribute_value)]):
+                raise ValueError(f"Student {student.id} has an invalid timeslot")
+
+        timeslot_coverage = 0.0
+        for team in team_set.teams:
+            num_mutual_timeslots = 0
+            for timeslot in self.available_timeslots:
+                if all(timeslot in student.attributes.get(self.timeslot_attribute_value) for student in team.students):
+                    num_mutual_timeslots += 1
+            timeslot_coverage += num_mutual_timeslots / float(len(self.available_timeslots))
+        return timeslot_coverage / float(len(team_set.teams))
