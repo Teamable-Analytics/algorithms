016058183f797b33c38eb08cf9b4393f62ab4c7f
justino599 <justin.schoenit@gmail.com>
2024-01-23 16:21:24 -0800

    Add save points util

--- benchmarking/runs/priority_algorithm/larger_simple_runs/run_utils.py
+++ benchmarking/runs/priority_algorithm/larger_simple_runs/run_utils.py
@@ -1,3 +1,7 @@
+from typing import List
+import json
+
+from benchmarking.evaluations.graphing.graph_3d import Surface3D
 from benchmarking.evaluations.interfaces import TeamSetMetric
 from benchmarking.evaluations.metrics.average_project_requirements_coverage import (
     AverageProjectRequirementsCoverage,
@@ -23,3 +27,10 @@ def get_pretty_metric_name(metric: TeamSetMetric) -> str:
         return "Project Coverage"
     if isinstance(metric, AverageSocialSatisfaction):
         return "Social Satisfaction"
+
+
+def save_points(surfaces: List[Surface3D], path: str):
+    points = {s.label: s.points for s in surfaces}
+
+    with open(f"{path}.json", "w+") as f:
+        json.dump(points, f)
