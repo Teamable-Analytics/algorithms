2bc10427ee392cbe0bbecf9446903f175f804452
justino599 <justin.schoenit@gmail.com>
2024-01-25 10:43:26 -0800

    try larger class

--- benchmarking/runs/priority_algorithm/default_parameters/runtimes.py
+++ benchmarking/runs/priority_algorithm/default_parameters/runtimes.py
@@ -18,10 +18,19 @@ from benchmarking.data.simulated_data.mock_student_provider import (
 )
 from benchmarking.evaluations.goals import DiversityGoal
 from benchmarking.evaluations.interfaces import Scenario, Goal
+from benchmarking.evaluations.metrics.average_social_satisfied import (
+    AverageSocialSatisfaction,
+)
+from benchmarking.evaluations.metrics.priority_satisfaction import PrioritySatisfaction
+from benchmarking.evaluations.metrics.utils.team_calculations import (
+    is_strictly_happy_team_friend,
+    is_happy_team_1hp_friend,
+)
 from benchmarking.evaluations.scenarios.include_social_friends import (
     IncludeSocialFriends,
 )
 from benchmarking.runs.interfaces import Run
+from benchmarking.simulation.goal_to_priority import goals_to_priorities
 from benchmarking.simulation.insight import Insight
 from benchmarking.simulation.simulation_set import SimulationSet
 from benchmarking.simulation.simulation_settings import SimulationSettings
@@ -29,7 +38,7 @@ from benchmarking.simulation.simulation_settings import SimulationSettings
 
 class Runtimes(Run):
     def start(self, num_trials: int = 100, generate_graphs: bool = False):
-        class_size = 120
+        class_sizes = [120, 1000]
         team_size = 4
 
         scenarios = {
@@ -37,55 +46,71 @@ class Runtimes(Run):
             "social": IncludeSocialFriends(max_num_friends=3, max_num_enemies=0),
         }
 
-        student_provider = MockStudentProvider(
-            settings=MockStudentProviderSettings(
-                number_of_students=class_size,
-                friend_distribution="cluster",
-                number_of_friends=3,
-                attribute_ranges={
-                    ScenarioAttribute.GENDER.value: [
-                        (Gender.FEMALE, 0.5),
-                        (Gender.MALE, 0.5),
-                    ],
-                    ScenarioAttribute.GPA.value: [
-                        (Gpa.A, 0.5),
-                        (Gpa.B, 0.5),
-                    ],
-                },
+        for class_size in class_sizes:
+            print(f"Class size: {class_size}")
+            student_provider = MockStudentProvider(
+                settings=MockStudentProviderSettings(
+                    number_of_students=class_size,
+                    friend_distribution="cluster",
+                    number_of_friends=3,
+                    attribute_ranges={
+                        ScenarioAttribute.GENDER.value: [
+                            (Gender.FEMALE, 0.5),
+                            (Gender.MALE, 0.5),
+                        ],
+                        ScenarioAttribute.GPA.value: [
+                            (Gpa.A, 0.5),
+                            (Gpa.B, 0.5),
+                        ],
+                    },
+                )
             )
-        )
 
-        for scenario_name, scenario in scenarios.items():
-            artifact = SimulationSet(
-                settings=SimulationSettings(
-                    num_teams=class_size // team_size,
-                    scenario=scenario,
-                    student_provider=student_provider,
-                    cache_key=f"priority_algorithm/default_parameters/{scenario_name}",
-                ),
-                algorithm_set={
-                    AlgorithmType.PRIORITY: [
-                        PriorityAlgorithmConfig(
-                            MAX_KEEP=30,
-                            MAX_SPREAD=100,
-                            MAX_ITERATE=250,
-                            MAX_TIME=1000000,
-                            name=f"config_250-100-30",
-                        )
-                        if "social" in scenario_name
-                        else PriorityAlgorithmConfig(
-                            MAX_KEEP=15,
-                            MAX_SPREAD=30,
-                            MAX_ITERATE=30,
-                            MAX_TIME=1000000,
-                            name=f"config_30-30-15",
-                        ),
-                    ],
-                },
-            ).run(num_runs=num_trials)
+            for scenario_name, scenario in scenarios.items():
+                artifact = SimulationSet(
+                    settings=SimulationSettings(
+                        num_teams=class_size // team_size,
+                        scenario=scenario,
+                        student_provider=student_provider,
+                        cache_key=f"priority_algorithm/default_parameters/{scenario_name}/class_size_{class_size}",
+                    ),
+                    algorithm_set={
+                        AlgorithmType.PRIORITY: [
+                            PriorityAlgorithmConfig(
+                                MAX_KEEP=30,
+                                MAX_SPREAD=100,
+                                MAX_ITERATE=250,
+                                MAX_TIME=1000000,
+                                name=f"config_250-100-30",
+                            )
+                            if "social" in scenario_name
+                            else PriorityAlgorithmConfig(
+                                MAX_KEEP=15,
+                                MAX_SPREAD=30,
+                                MAX_ITERATE=30,
+                                MAX_TIME=1000000,
+                                name=f"config_30-30-15",
+                            ),
+                        ],
+                    },
+                ).run(num_runs=num_trials)
 
-            insight_output = Insight.get_output_set(artifact, [])
-            print(insight_output)
+                metrics = {
+                    "PrioritySatisfaction": PrioritySatisfaction(
+                        goals_to_priorities(scenario.goals),
+                        False,
+                    ),
+                    "AverageSocialSatisfaction": AverageSocialSatisfaction(
+                        metric_function=is_happy_team_1hp_friend,
+                    ),
+                }
+                insight_output = Insight.get_output_set(artifact, list(metrics.values()))
+                print(scenario_name + ":")
+                print(
+                    f"Runtime: {Insight.average_metric(insight_output, Insight.KEY_RUNTIMES)}"
+                )
+                for name in metrics.keys():
+                    print(f"{name}: {Insight.average_metric(insight_output, name)}")
 
 
 class OneDiversityScenario(Scenario):
@@ -106,39 +131,6 @@ class OneDiversityScenario(Scenario):
                     direction=TokenizationConstraintDirection.MIN_OF,
                     threshold=2,
                     value=self.value_of_female,
-                ),
-            ),
-        ]
-
-
-class TwoDiversityScenario(Scenario):
-    def __init__(self, value_of_female: int, value_of_a: int):
-        self.value_of_female = value_of_female
-        self.value_of_a = value_of_a
-
-    @property
-    def name(self) -> str:
-        return "Scenario with one diversity goal"
-
-    @property
-    def goals(self) -> List[Goal]:
-        return [
-            DiversityGoal(
-                DiversifyType.DIVERSIFY,
-                ScenarioAttribute.GENDER.value,
-                tokenization_constraint=TokenizationConstraint(
-                    direction=TokenizationConstraintDirection.MIN_OF,
-                    threshold=2,
-                    value=self.value_of_female,
-                ),
-            ),
-            DiversityGoal(
-                DiversifyType.DIVERSIFY,
-                ScenarioAttribute.GPA.value,
-                tokenization_constraint=TokenizationConstraint(
-                    direction=TokenizationConstraintDirection.MIN_OF,
-                    threshold=2,
-                    value=self.value_of_a,
                 ),
             ),
         ]
