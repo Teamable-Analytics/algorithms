192cd4ab5d6ee6ab3cc756c2ca522a027b032f95
ketphan02 <tuankietvn@outlook.com>
2024-02-23 19:58:56 -0800

    Address comments

--- benchmarking/evaluations/metrics/average_solo_status.py
+++ benchmarking/evaluations/metrics/average_solo_status.py
@@ -7,14 +7,21 @@ from benchmarking.evaluations.interfaces import TeamSetMetric
 
 class AverageSoloStatus(TeamSetMetric):
     """
-    Calculate the average number of students having solo status in a team
+    Calculate the average number of students having solo status in a team. A student is only considered as solo status
+    once, even if they have multiple minority attributes.
+
+    Params
+    ------
+    minority_groups: Dict[int, List[int]]
+        A dictionary where the key is the attribute_id and the value is a list of attribute_values that are considered
+        as minority.
     """
 
-    def __init__(self, minority_groups: Dict[int, List[int]], *args, **kwargs):
+    def __init__(self, minority_groups_map: Dict[int, List[int]], *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.minority_groups = minority_groups
+        self.minority_groups_map = minority_groups_map
 
-        if len(minority_groups) == 0:
+        if len(minority_groups_map) == 0:
             raise ValueError("minority_groups cannot be empty")
 
     def calculate(self, team_set: TeamSet) -> float:
@@ -23,7 +30,7 @@ class AverageSoloStatus(TeamSetMetric):
         for team in team_set.teams:
             for student in team.students:
                 total_students += 1
-                for attribute_id, attribute_groups in self.minority_groups.items():
+                for attribute_id, attribute_groups in self.minority_groups_map.items():
                     # Create a dictionary of attribute_values and number of appearance in the team
                     all_attribute_values_in_team = Counter(
                         [
@@ -35,7 +42,7 @@ class AverageSoloStatus(TeamSetMetric):
 
                     for attr in student.attributes.get(attribute_id):
                         if (
-                            attr in self.minority_groups.get(attribute_id)
+                            attr in self.minority_groups_map.get(attribute_id)
                             and all_attribute_values_in_team.get(attr) == 1
                         ):
                             total_solo_status_students.add(student.id)
