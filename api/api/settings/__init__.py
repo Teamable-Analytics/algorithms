7d9419ad00ed5bf1e55d24eb466dc44cf8d75a93
ketphan02 <tuankietvn@outlook.com>
2024-02-24 20:07:24 -0800

    CORS

--- api/api/settings/__init__.py
+++ api/api/settings/__init__.py
@@ -27,6 +27,13 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
+if DEBUG:
+    CORS_ALLOWED_ORIGINS = [
+        "http://localhost:8080",
+    ]
+
+    CORS_ALLOW_HEADERS = "*"
+
 
 # Application definition
 
@@ -37,6 +44,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "api",
+    "corsheaders",
     "rest_framework",
 ]
 
@@ -48,6 +56,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "api.api.middlewares.api_key_authentication.APIKeyAuthenticationMiddleware",
 ]
 
