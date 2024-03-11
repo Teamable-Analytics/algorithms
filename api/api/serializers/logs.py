4e14c541f22d70ac2f56aa06da431c21b34a9f9f
opeyem1a <opeyadeyemi@gmail.com>
2024-03-11 00:28:06 -0600

    remove unused api logs generate serializer

--- api/api/serializers/logs.py
+++ api/api/serializers/logs.py
@@ -1,15 +1,9 @@
 from rest_framework import serializers
 
-from api.api.models import APILog, APILogGenerate
+from api.api.models import APILog
 
 
 class APILogSerializer(serializers.ModelSerializer):
     class Meta:
         model = APILog
-        fields = "__all__"
-
-
-class APILogGenerateSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = APILogGenerate
         fields = "__all__"
