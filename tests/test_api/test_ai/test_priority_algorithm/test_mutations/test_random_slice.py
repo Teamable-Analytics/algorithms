a3faa570febe37b01b2960db9fec8249c9518aed
justino599 <justin.schoenit@gmail.com>
2024-03-11 10:24:16 -0700

    Add test

---
+++ tests/test_api/test_ai/test_priority_algorithm/test_mutations/test_random_slice.py
@@ -0,0 +1,54 @@
+import unittest
+
+from api.ai.priority_algorithm.custom_models import PriorityTeamSet, PriorityTeam
+from api.ai.priority_algorithm.mutations.random_slice import mutate_random_slice
+from api.models.team import TeamShell
+from benchmarking.data.simulated_data.mock_student_provider import (
+    MockStudentProvider,
+    MockStudentProviderSettings,
+)
+
+
+class TestRandomSlice(unittest.TestCase):
+    def test_mutate__changes_one_student_in_each_team_set(self):
+        students = MockStudentProvider(
+            settings=MockStudentProviderSettings(
+                number_of_students=100,
+            )
+        ).get()
+        team_set = PriorityTeamSet(
+            priority_teams=[
+                PriorityTeam(
+                    student_ids=[
+                        student.id
+                        for student in students[team_id * 5 : team_id * 5 + 5]
+                    ],
+                    team_shell=TeamShell(team_id),
+                )
+                for team_id in range(20)
+            ]
+        )
+        initial_team_set = team_set.clone()
+        mutate_random_slice(team_set)
+
+        num_teams_changed = 0
+        for initial_team, mutated_team in zip(
+            initial_team_set.priority_teams, team_set.priority_teams
+        ):
+            self.assertEqual(initial_team.team_shell.id, mutated_team.team_shell.id)
+
+            self.assertEqual(
+                len(initial_team.student_ids), len(mutated_team.student_ids)
+            )
+
+            # Check that the teams are different,
+            # but there is a chance that a student gets put back on the same team
+
+            # Gives the new student(s) in the mutated team
+            diff = list(set(mutated_team.student_ids) - set(initial_team.student_ids))
+            num_students_changed = len(diff)
+            num_teams_changed += 1 if num_students_changed > 0 else 0
+            self.assertTrue(num_students_changed in [0, 1])
+
+        # Technically flaky, but it's a 1 in 104,857,600,000,000,000,000,000,000 chance to fail
+        self.assertGreater(num_teams_changed, 0)
