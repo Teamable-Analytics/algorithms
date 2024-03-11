1d3a972073003275eb8ce8c808b5e24fd8a2d2dd
ketphan02 <tuankietvn@outlook.com>
2024-03-11 16:58:08 -0700

    Scenarios we used in paper that are related to Group Matcher algorithm

---
+++ benchmarking/evaluations/scenarios/concentrate_timeslots_and_concentrate_gender_and_concentrate_race.py
@@ -0,0 +1,48 @@
+from typing import List
+
+from api.dataclasses.enums import (
+    DiversifyType,
+    ScenarioAttribute,
+    Gender,
+    Race,
+)
+from benchmarking.evaluations.goals import WeightGoal, DiversityGoal
+from benchmarking.evaluations.interfaces import Scenario, Goal
+
+
+class ConcentrateTimeslotsAndConcentrateGenderAndConcentrateRace(Scenario):
+    def __init__(
+        self,
+        max_num_choices: int,
+        value_of_female: int = Gender.FEMALE,
+        value_of_african: int = Race.African,
+    ):
+        super().__init__()
+        self.value_of_female = value_of_female
+        self.value_of_african = value_of_african
+        self.max_num_choices = max_num_choices
+
+    @property
+    def name(self):
+        return "Concentrate on Timeslot and Concentrate Gender and Concentrate Race"
+
+    @property
+    def goals(self) -> List[Goal]:
+        return [
+            DiversityGoal(
+                DiversifyType.CONCENTRATE,
+                ScenarioAttribute.TIMESLOT_AVAILABILITY.value,
+                max_num_choices=self.max_num_choices,
+            ),
+            DiversityGoal(
+                DiversifyType.CONCENTRATE,
+                ScenarioAttribute.GENDER.value,
+                max_num_choices=1,
+            ),
+            DiversityGoal(
+                DiversifyType.CONCENTRATE,
+                ScenarioAttribute.RACE.value,
+                max_num_choices=1,
+            ),
+            WeightGoal(diversity_goal_weight=1),
+        ]
---
+++ benchmarking/evaluations/scenarios/satisfy_project_requirements.py
@@ -0,0 +1,20 @@
+from typing import List
+
+from api.dataclasses.enums import RequirementsCriteria
+from benchmarking.evaluations.goals import WeightGoal, ProjectRequirementGoal
+from benchmarking.evaluations.interfaces import Scenario, Goal
+
+
+class SatisfyProjectRequirements(Scenario):
+    @property
+    def name(self) -> str:
+        return "Satisfy Project Requirements"
+
+    @property
+    def goals(self) -> List[Goal]:
+        return [
+            ProjectRequirementGoal(
+                criteria=RequirementsCriteria.PROJECT_REQUIREMENTS_ARE_SATISFIED
+            ),
+            WeightGoal(project_requirement_weight=1),
+        ]
