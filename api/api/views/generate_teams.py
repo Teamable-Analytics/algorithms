8d66ef17370b5753c4b01bbda254a5d43ad002e3
justino599 <justin.schoenit@gmail.com>
2024-03-19 11:51:21 -0700

    Log /generate/teams calls and add sandbox query param

--- api/api/views/generate_teams.py
+++ api/api/views/generate_teams.py
@@ -1,9 +1,11 @@
 from rest_framework import viewsets
 from rest_framework.decorators import action
+from rest_framework.request import Request
 from schema import SchemaError
 
 from api.ai.algorithm_runner import AlgorithmRunner
 from api.api.utils.generate_teams_data_loader import GenerateTeamsDataLoader
+from api.api.utils.logger import APILogger
 from api.api.utils.response_with_metadata import ResponseWithMetadata
 from api.api.validators.generate_teams_validator import GenerateTeamsValidator
 from api.dataclasses.team_set.serializer import TeamSetSerializer
@@ -11,13 +13,28 @@ from api.dataclasses.team_set.serializer import TeamSetSerializer
 
 class GenerateTeamsViewSet(viewsets.GenericViewSet):
     @action(url_path="teams", detail=False, methods=["POST"])
-    def generate_teams(self, request):
+    def generate_teams(self, request: Request):
         try:
             request_data = dict(request.data)
 
             # validate data schema
             GenerateTeamsValidator(request_data).validate()
             input_data = GenerateTeamsDataLoader(request_data).load()
+
+            # Lowercase the params
+            params = {k.lower(): v for k, v in request.query_params.items()}
+            # Don't log if it is a sandbox call
+            if not params.get("sandbox") or params.get("sandbox").lower() != "true":
+                # Log API call. Do it after validation has completed.
+                APILogger.log(
+                    endpoint="/generate/teams",
+                    data={
+                        "algorithm_options": request_data.get("algorithm_options"),
+                        "team_generation_options": request_data.get(
+                            "team_generation_options"
+                        ),
+                    },
+                )
 
             runner = AlgorithmRunner(
                 algorithm_type=input_data.algorithm_type,
