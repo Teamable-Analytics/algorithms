bd6389e9c04229a0e5d4d146a1f794884679c298
Seth Akins <sethlinc4@gmail.com>
2024-03-28 12:44:03 -0700

    Implement Mutation to Randomly Change Team Sizes (#414)
    * Refactored mutations to new class
    
    * Refactored runs to use new mutation structure
    
    * Small bug fixes
    
    * Move Mutation and MutationSet to interfaces.py
    
    * Commit over interface to make Justin get github points.
    We introduced a new refactor to make Opey happy (we hope).
    
    * Started on variable size team set refactor
    
    * Finished refactor
    
    * Refactored mutations to accept a min and max team size
    
    * Started working on random team set size mutations
    
    * Finished mutation
    
    * Addressed requested changes
    
    ---------
    
    Co-authored-by: justino599 <justin.schoenit@gmail.com>

---
+++ api/ai/priority_algorithm/mutations/random_team_size.py
@@ -0,0 +1,62 @@
+import random
+from typing import Dict, List
+
+from api.ai.interfaces.team_generation_options import TeamGenerationOptions
+from api.ai.priority_algorithm.custom_dataclasses import PriorityTeamSet
+from api.ai.priority_algorithm.mutations.interfaces import Mutation
+from api.ai.priority_algorithm.mutations.utils import get_available_priority_teams
+from api.ai.priority_algorithm.priority.interfaces import Priority
+from api.dataclasses.student import Student
+
+
+class RandomTeamSizeMutation(Mutation):
+    def __init__(self, number_of_teams: int = 2, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.number_of_teams = number_of_teams
+
+    def validate(self):
+        if self.number_of_teams < 2:
+            raise ValueError("Random team size mutation must have at least two teams")
+
+    def mutate_one(
+        self,
+        priority_team_set: PriorityTeamSet,
+        priorities: List[Priority],
+        student_dict: Dict[int, Student],
+        team_generation_options: TeamGenerationOptions,
+    ) -> PriorityTeamSet:
+        if (
+            team_generation_options.min_team_size
+            == team_generation_options.max_team_size
+        ):
+            return priority_team_set
+        try:
+            available_priority_teams = get_available_priority_teams(priority_team_set)
+            if len(available_priority_teams) < self.number_of_teams:
+                return priority_team_set
+            teams = random.sample(available_priority_teams, self.number_of_teams)
+            movable_students = [
+                student_id
+                for team in teams
+                for student_id in team.student_ids[
+                    team_generation_options.min_team_size :
+                ]
+            ]
+            for team in teams:
+                team.student_ids = team.student_ids[
+                    : team_generation_options.min_team_size
+                ]
+            random.shuffle(movable_students)
+            for student in movable_students:
+                index = random.randrange(0, self.number_of_teams)
+                for i in range(self.number_of_teams):
+                    j = (index + i) % self.number_of_teams
+                    if (
+                        len(teams[j].student_ids)
+                        < team_generation_options.max_team_size
+                    ):
+                        teams[j].student_ids.append(student)
+                        break
+        except ValueError:
+            return priority_team_set
+        return priority_team_set
