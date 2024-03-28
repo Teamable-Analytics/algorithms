60f25f91fff4f15fed5c465cc2424c164027a1e0
justino599 <justin.schoenit@gmail.com>
2024-03-27 21:04:51 -0700

    Add team size mutation that minimizes team disruption

---
+++ api/ai/priority_algorithm/mutations/team_size_low_disruption.py
@@ -0,0 +1,95 @@
+from typing import List, Dict, Tuple
+
+import numpy as np
+
+from api.ai.interfaces.team_generation_options import TeamGenerationOptions
+from api.ai.priority_algorithm.custom_dataclasses import PriorityTeamSet, PriorityTeam
+from api.ai.priority_algorithm.mutations.interfaces import Mutation
+from api.ai.priority_algorithm.mutations.utils import get_available_priority_teams
+from api.ai.priority_algorithm.priority.interfaces import Priority
+from api.dataclasses.student import Student
+
+
+class TeamSizeLowDisruptionMutation(Mutation):
+    def mutate_one(
+        self,
+        priority_team_set: PriorityTeamSet,
+        priorities: List[Priority],
+        student_dict: Dict[int, Student],
+        team_generation_options: TeamGenerationOptions,
+    ) -> PriorityTeamSet:
+        """
+        Mutation to adjust team sizes within the bounds.
+
+        The idea is to first randomly generate the sizes of the new teams (with approx uniform distribution).
+        Then, only move students from teams with too many students to teams with not enough students.
+        This minimizes the change in teams and hopefully won't tear apart teams that are doing well.
+        """
+
+        available_priority_teams = get_available_priority_teams(priority_team_set)
+        try:
+            if len(available_priority_teams) < 2:
+                return priority_team_set
+
+            new_sizes = get_sizes(
+                len(available_priority_teams),
+                sum([len(team.student_ids) for team in available_priority_teams]),
+                team_generation_options.min_team_size,
+                team_generation_options.max_team_size,
+            )
+
+            # Sort teams into those which need to shrink, need to grow, and are the right size
+            shrinking_teams: List[Tuple[int, PriorityTeam]] = []
+            growing_teams: List[Tuple[int, PriorityTeam]] = []
+            done_teams: List[PriorityTeam] = []
+            for size, team in zip(new_sizes, available_priority_teams):
+                if len(team.student_ids) > size:
+                    shrinking_teams.append((size, team))
+                elif len(team.student_ids) < size:
+                    growing_teams.append((size, team))
+                else:
+                    done_teams.append(team)
+
+            # Move students from oversized teams to undersized teams
+            for target_size, growing_team in growing_teams:
+                while len(growing_team.student_ids) < target_size:
+                    # Grab a random student from the last team in the shrinking teams
+                    shrink_target, shrinking_team = shrinking_teams[0]
+                    growing_team.student_ids.append(
+                        shrinking_team.student_ids.pop(
+                            np.random.randint(len(shrinking_team.student_ids))
+                        )
+                    )
+                    if len(shrinking_team.student_ids) == shrink_target:
+                        # Move shrinking team to done
+                        done_teams.append(shrinking_team)
+                        shrinking_teams.pop(0)
+                done_teams.append(growing_team)
+        except ValueError:
+            return priority_team_set
+        return priority_team_set
+
+
+def get_sizes(
+    num_teams: int, num_students: int, min_size: int, max_size: int
+) -> List[int]:
+    """
+    Generates a list of team sizes
+    """
+    sizes = [np.random.randint(min_size, max_size + 1) for _ in range(num_teams)]
+    diff = num_students - sum(sizes)
+    if diff < 0:
+        # If the teams sizes are bigger than the number of students
+        for _ in range(abs(diff)):
+            index = np.random.randint(num_teams)
+            while sizes[index] <= min_size:
+                index = np.random.randint(num_teams)
+            sizes[index] -= 1
+    elif diff > 0:
+        # If the teams sizes are smaller than the number of students
+        for _ in range(diff):
+            index = np.random.randint(num_teams)
+            while sizes[index] >= max_size:
+                index = np.random.randint(num_teams)
+            sizes[index] += 1
+    return sizes
---
+++ tests/test_api/test_ai/test_priority_algorithm/test_mutations/test_team_size_low_disruption.py
@@ -0,0 +1,98 @@
+import unittest
+
+from api.ai.priority_algorithm.custom_dataclasses import PriorityTeamSet, PriorityTeam
+from api.ai.priority_algorithm.mutations.team_size_low_disruption import (
+    get_sizes,
+    TeamSizeLowDisruptionMutation,
+)
+from api.dataclasses.team import TeamShell
+from benchmarking.data.simulated_data.mock_student_provider import (
+    MockStudentProvider,
+    MockStudentProviderSettings,
+)
+from benchmarking.simulation.mock_algorithm import MockAlgorithm
+from tests.test_api.test_ai.test_priority_algorithm.test_mutations._data import (
+    get_mock_student_dict,
+)
+
+
+class TestTeamSizeLowDisruptionMutation(unittest.TestCase):
+    def setUp(self):
+        students = MockStudentProvider(
+            settings=MockStudentProviderSettings(
+                number_of_students=50,
+            )
+        ).get()
+        self.student_dict = get_mock_student_dict(students)
+        student_ids = list(self.student_dict.keys())
+        self.team_set = PriorityTeamSet(
+            priority_teams=[
+                PriorityTeam(
+                    student_ids=student_ids[i : i + 5], team_shell=TeamShell(i)
+                )
+                for i in range(10)
+            ]
+        )
+        self.students = students
+        self.mutation = TeamSizeLowDisruptionMutation()
+        self.team_generation_options = MockAlgorithm.get_team_generation_options(
+            num_students=50, num_teams=10, min_team_size=3, max_team_size=7
+        )
+
+    def test_get_sizes__sums_to_num_students(self):
+        sizes = get_sizes(100, 500, 3, 6)
+        self.assertEqual(500, sum(sizes))
+
+    def test_get_sizes__sizes_are_in_range(self):
+        sizes = get_sizes(100, 500, 3, 6)
+        for size in sizes:
+            self.assertLessEqual(size, 6)
+            self.assertGreaterEqual(size, 3)
+
+    def test_get_sizes__len_sizes_equals_num_teams(self):
+        sizes = get_sizes(100, 500, 3, 6)
+        self.assertEqual(100, len(sizes))
+
+    def test_mutate_one__returns_correct_type(self):
+        mutated_team_set = self.mutation.mutate_one(
+            self.team_set.clone(),
+            [],
+            self.student_dict,
+            self.team_generation_options,
+        )
+        self.assertIsInstance(mutated_team_set, PriorityTeamSet)
+
+    def test_mutate_one__returns_all_students(self):
+        mutated_team_set = self.mutation.mutate_one(
+            self.team_set.clone(),
+            [],
+            self.student_dict,
+            self.team_generation_options,
+        )
+        original_students = [
+            student
+            for team in self.team_set.priority_teams
+            for student in team.student_ids
+        ]
+        returned_students = [
+            student
+            for team in mutated_team_set.priority_teams
+            for student in team.student_ids
+        ]
+        self.assertEqual(len(self.students), len(returned_students))
+        self.assertEqual(set(original_students), set(returned_students))
+
+    def test_mutate_one__team_sizes_are_in_bounds(self):
+        mutated_team_set = self.mutation.mutate_one(
+            self.team_set.clone(),
+            [],
+            self.student_dict,
+            self.team_generation_options,
+        )
+        for team in mutated_team_set.priority_teams:
+            self.assertLessEqual(
+                len(team.student_ids), self.team_generation_options.max_team_size
+            )
+            self.assertGreaterEqual(
+                len(team.student_ids), self.team_generation_options.min_team_size
+            )
